deeaa24402c5182052686fe8e4e0276fc76d8b9b
Betterbird <betterbird@betterbird.eu>
2022-04-06 21:16:53 +0200

    Added additional Dutch translations, contribution of Roy Kroeze.

--- scripts/nl.cmd
+++ scripts/nl.cmd
@@ -15,6 +15,16 @@ set lecho=C:\mozilla-build\msys\bin\lessecho.exe
 :: <!ENTITY contextDecryptToFolder.accesskey "Y">
 %lecho% "<!ENTITY contextDecryptToFolder.label \"Ontsleutelen naar map\">" >> chrome\nl\locale\nl\messenger\messenger.dtd
 %lecho% "<!ENTITY contextDecryptToFolder.accesskey \"a\">" >> chrome\nl\locale\nl\messenger\messenger.dtd
+:: decryptToFolder.title=Confirm Decrypt
+:: decryptToFolder.desc=This will decrypt messages to a folder which is not local. Are you sure you want to continue?
+:: decryptToFolder.dontAsk.label=Don't ask me again.
+%lecho% "decryptToFolder.title=Ontsleuteling bevestigen" >> chrome\nl\locale\nl\messenger\messenger.properties
+%lecho% "decryptToFolder.desc=Dit zal het bericht ontsleutelen naar een niet-lokale map. Weet u zeker dat u door wilt gaan?" >> chrome\nl\locale\nl\messenger\messenger.properties
+%lecho% "decryptToFolder.dontAsk.label=Niet opnieuw vragen." >> chrome\nl\locale\nl\messenger\messenger.properties
+
+:: Original string:
+:: <!ENTITY folderContextStartupFolder.label "Startup Folder">
+%lecho% "<!ENTITY folderContextStartupFolder.label \"Start map\">" >> chrome\nl\locale\nl\messenger\messenger.dtd
 
 :: Original strings:
 :: <!ENTITY quickFilterBar.untagged.tooltip "Show messages with no tags">
@@ -27,6 +37,16 @@ set lecho=C:\mozilla-build\msys\bin\lessecho.exe
 ::     .label = Enable search in encrypted messages (stores decrypted copy locally)
 %lecho% "enable-gloda-search-encrypted-label =" >> localization\nl\messenger\preferences\preferences.ftl
 %lecho% "    .label = Versleutelde berichten doorzoekbaar maken (slaat ontsleutelde kopie lokaal op)" >> localization\nl\messenger\preferences\preferences.ftl
+
+:: Original strings:
+:: menuitem-recent-files =
+::     .label = Recent File(s)…
+:: context-menuitem-recent-files =
+::     .label = Attach Recent File(s)…
+%lecho% "menuitem-recent-files ="                   >> localization\nl\messenger\messengercompose\messengercompose.ftl
+%lecho% "    .label = Recent geopende bestand(en)…" >> localization\nl\messenger\messengercompose\messengercompose.ftl
+%lecho% "context-menuitem-recent-files ="           >> localization\nl\messenger\messengercompose\messengercompose.ftl
+%lecho% "    .label = Voeg recent geopende bestand(en) toe…" >> localization\nl\messenger\messengercompose\messengercompose.ftl
 
 :: Original strings:
 :: <!ENTITY multilineAllFolders.label "Multi-line View On All Folders">
