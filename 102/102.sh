c207696d5226108d1737c15e0ea02ea5355cf0eb
Betterbird <betterbird@betterbird.eu>
2022-05-07 11:40:18 +0200

    [102] Add 102.sh with repos/revs for version 102.

---
+++ 102/102.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+mozilla_repo=https://hg.mozilla.org/mozilla-central/
+mozilla_rev=fe0b18ac5fe152cad89a780b5e48c2b325fea839
+comm_repo=https://hg.mozilla.org/comm-central/
+comm_rev=1a9b35ab2d54efc3ce65c59a2617c9752bf81481
