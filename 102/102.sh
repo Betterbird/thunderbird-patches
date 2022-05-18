fa42e1c54c13f761c442bc16e115a4b337fa35de
Betterbird <betterbird@betterbird.eu>
2022-05-18 22:33:55 +0200

    [102] Added mozconfigs and rebased

--- 102/102.sh
+++ 102/102.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mozilla_repo=https://hg.mozilla.org/mozilla-central/
-mozilla_rev=05f61ce1fd54d7abed9e22b0bd5f4b540ff790b9
+mozilla_rev=90328ce5bee2f13de45bdb402054e0288abf7c05
 comm_repo=https://hg.mozilla.org/comm-central/
 comm_rev=97918d20311ca21acc36bb4327b8ff4ed652f91c
