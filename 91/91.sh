be4276c18c90c44ab15a5b5b4e9f4d80e8919bab
Betterbird <betterbird@betterbird.eu>
2022-05-26 15:15:33 +0200

    Update to official C-ESR91 version.

--- 91/91.sh
+++ 91/91.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr91/
 MOZILLA_REV=9cbe8ffa12101019dbe0da698f869ed3ef0b9370
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr91/
-COMM_REV=0367964cd1580546c946471868293f72e0b03122
+COMM_REV=4a4b901410a9055d078d0cdd7c62b2b5d56ffbad
 RUST_VER=1.53.0
