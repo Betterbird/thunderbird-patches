3a0fc8b82c1aa4f467b2b1480fdd292208b53359
Betterbird <betterbird@betterbird.eu>
2022-06-01 22:35:48 +0200

    [102] Rebase to NIGHTLY_102_END in preparation for 102 beta.

--- 102/102.sh
+++ 102/102.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
-MOZILLA_REV=b113470be0ad3088a006850b42b9d7c4b8d409bb
+MOZILLA_REV=7db3b1cf976f9634d567201a8259bed4ba14fe5c
 COMM_REPO=https://hg.mozilla.org/comm-central/
-COMM_REV=90328ce5bee2f13de45bdb402054e0288abf7c05
-RUST_VER=1.102.0
+COMM_REV=ab5f28b4f4f6a85ed863a25ebec137743a7ca2fe
+RUST_VER=1.60.0
 
