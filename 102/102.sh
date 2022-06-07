a057e5c02af186e57a551393fb0cd9297ddf34f1
Betterbird <betterbird@betterbird.eu>
2022-06-07 08:46:19 +0200

    [102] Rebase to 102.0b3.

--- 102/102.sh
+++ 102/102.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
-MOZILLA_REV=ade0a99ab82151dcf6bd94dd207896f252a85bb3
+MOZILLA_REV=a6364dc1376bc4c905722a9f8eafa94be8591628
 COMM_REPO=https://hg.mozilla.org/releases/comm-beta/
-COMM_REV=48df8d17f07b73d9237f99865f23f901bc1d53c5
+COMM_REV=7208b045160b302c9fa8d82a40994de4d109556a
 RUST_VER=1.60.0
 
