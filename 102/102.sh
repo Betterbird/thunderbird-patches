303802cb5ab0865f92d3e5b28caf2ca414f33151
Betterbird <betterbird@betterbird.eu>
2022-06-17 20:41:56 +0200

    [102 ] Rebase to 102.0b7, fold patches for bug 683809 (QF untagged messages).

--- 102/102.sh
+++ 102/102.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
-MOZILLA_REV=6c0dfa7b9ff09c13679ff4a3668c197f72d118ea
+MOZILLA_REV=7961a601ee114ff704081a34f180e191ec754379
 COMM_REPO=https://hg.mozilla.org/releases/comm-beta/
-COMM_REV=bfe3e15a4cd571ffc84a838d1f27d5172f2cd8b9
+COMM_REV=eae24122010b8257a66e5a68c7b0231ed044e9e2
 RUST_VER=1.60.0
-
