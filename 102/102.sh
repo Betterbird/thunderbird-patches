8845c728941492bfcef5eecb8278faa8ee302467
Betterbird <betterbird@betterbird.eu>
2022-06-24 00:24:43 +0200

    [102] Switch 102 from beta to ESR, rebase.

--- 102/102.sh
+++ 102/102.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
-MOZILLA_REV=085af07e5cc10082e793203e049ee4d6127a452f
-COMM_REPO=https://hg.mozilla.org/releases/comm-beta/
-COMM_REV=4d1977bafbfe785c167fee18ff3d96d58761b603
+MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
+MOZILLA_REV=4180ec744bb748a5fd39e25d122c4b500c3a2a00
+COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
+COMM_REV=de05ef44a8e2a1182705cc5e235b9a8642f96e4d
 RUST_VER=1.60.0
