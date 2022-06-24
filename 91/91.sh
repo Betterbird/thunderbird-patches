289efe5f668697f5a022cfe08f3ccdeef850ae71
Betterbird <betterbird@betterbird.eu>
2022-06-24 20:50:59 +0200

    [91] Prepare for 91.11.0-bb34.

--- 91/91.sh
+++ 91/91.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr91/
-MOZILLA_REV=9cbe8ffa12101019dbe0da698f869ed3ef0b9370
+MOZILLA_REV=cec32c7ec3acf7e8f2afa89ee287b2c6f5e6db81
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr91/
-COMM_REV=4a4b901410a9055d078d0cdd7c62b2b5d56ffbad
+COMM_REV=8d172cc2dd982eccc7efd25640971eff06a8fb55
 RUST_VER=1.53.0
