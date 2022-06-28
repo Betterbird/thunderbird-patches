53f7769797864f5744b1acc66091b1af0ec5ea80
Betterbird <betterbird@betterbird.eu>
2022-06-28 21:01:28 +0200

    [102] Rebase to TB 102.0 RC 2.

--- 102/102.sh
+++ 102/102.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
 MOZILLA_REV=4180ec744bb748a5fd39e25d122c4b500c3a2a00
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=de05ef44a8e2a1182705cc5e235b9a8642f96e4d
+COMM_REV=fecf1316a2f2373d6affc7b72080de95dd74fb48
 RUST_VER=1.60.0
