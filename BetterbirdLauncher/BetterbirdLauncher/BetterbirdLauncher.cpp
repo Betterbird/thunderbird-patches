87ae2b0c3605e6a17ad4a017dbad80ef41e9e2b4
Betterbird <betterbird@betterbird.eu>
2022-07-05 12:25:54 +0200

    [Launcher] Delete addonStartup.json.lz4 before launching the executable.

--- BetterbirdLauncher/BetterbirdLauncher/BetterbirdLauncher.cpp
+++ BetterbirdLauncher/BetterbirdLauncher/BetterbirdLauncher.cpp
@@ -1,10 +1,13 @@
 #include <shlobj.h>
 #include <shlwapi.h>
 #include <objbase.h>
+#include <windows.h>
+#include <stdio.h>
 #define MAX_PATH_PROFILE MAX_PATH + 50
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
   TCHAR appPath[MAX_PATH];
+  TCHAR cacheFile[MAX_PATH];
   TCHAR profilePath[MAX_PATH_PROFILE];
   SHELLEXECUTEINFO ShExecInfo;
 
@@ -14,10 +17,20 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
   *lastSlash = 0;
 
   // Prepare profile folder path.
-  wcscpy_s(profilePath, appPath);
   wcscpy_s(profilePath, L"-new-instance -profile \"");
   wcscat_s(profilePath, MAX_PATH_PROFILE, appPath);
   wcscat_s(profilePath, MAX_PATH_PROFILE, L"\\profile\"");
+
+  // Path to the cache file we want to delete.
+  wcscpy_s(cacheFile, appPath);
+  wcscat_s(cacheFile, MAX_PATH_PROFILE, L"\\profile\\addonStartup.json.lz4");
+  bool res = DeleteFileW(cacheFile);
+#if 0
+  DWORD err = GetLastError();
+  char msg[MAX_PATH + 100];
+  sprintf_s(msg, sizeof(msg), "BetterbirdLauncher: Deleted %S with success %d %lx\n", cacheFile, res, err);
+  OutputDebugStringA(msg);
+#endif
 
   // Add executable.
   wcscat_s(appPath, MAX_PATH, L"\\core\\betterbird.exe");
