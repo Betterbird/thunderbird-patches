2c6ae07653d6e6604ffe787e4a2511b5b64b9799
Betterbird <betterbird@betterbird.eu>
2022-07-05 22:54:20 +0200

    [102] Rebase to official C-ESR102 version for 102.0.1.

--- 102/102.sh
+++ 102/102.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
 MOZILLA_REV=4180ec744bb748a5fd39e25d122c4b500c3a2a00
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=7f97b55f9e2cdcf45239c47e9f49b1ecf04c887d
+COMM_REV=7d5734d2820f9abcda8e04d6f9c98193249b2c43
 RUST_VER=1.60.0
