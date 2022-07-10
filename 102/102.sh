97157b6e308f099ec4ddeccd01d1e710208002eb
Betterbird <betterbird@betterbird.eu>
2022-07-10 21:42:23 +0200

    [102] Prepare for 102.0.2-bb10, rease two patches.

--- 102/102.sh
+++ 102/102.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
 MOZILLA_REV=4180ec744bb748a5fd39e25d122c4b500c3a2a00
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=7d5734d2820f9abcda8e04d6f9c98193249b2c43
+COMM_REV=1357fff4c27503ee29b594ab715648f48af3bd45
 RUST_VER=1.60.0
