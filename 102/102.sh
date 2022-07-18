0a0159ec96b7dca865112eb9702cbff2c823b83a
Betterbird <betterbird@betterbird.eu>
2022-07-18 21:16:12 +0200

    [102] Prepare for 102.0.3-bb11, rebase one patch.

--- 102/102.sh
+++ 102/102.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
-MOZILLA_REV=4180ec744bb748a5fd39e25d122c4b500c3a2a00
+MOZILLA_REV=0faf039283ce419e48c558b422bd6d9c1c882c75
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=1357fff4c27503ee29b594ab715648f48af3bd45
+COMM_REV=ce1a90901b88ff77c156071e8b13a4c2513e758b
 RUST_VER=1.60.0
