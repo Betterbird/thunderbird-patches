9636ca7734923469f923c670e14da42f90192806
Betterbird <betterbird@betterbird.eu>
2022-08-04 20:36:02 +0200

    [102] Prepare for 102.1.1-bb13, take 2: Use offical ESR changeset and rebase two patches.

--- 102/102.sh
+++ 102/102.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
 MOZILLA_REV=73db4126165f2a2eded92f48a6c81c8ece6d21ce
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=288a59f206393b0f6b1957619dc95f5bdd700de3
+COMM_REV=48f3d2e53b8d6ec1dab03a6a9923c845ea69c583
 RUST_VER=1.60.0
