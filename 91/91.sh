68d09e078f0dfdcfeea6118ca2ba65a5f6928352
Betterbird <betterbird@betterbird.eu>
2022-08-18 17:12:06 +0200

    [91] Prepare for 91.13.0-bb36, rebase two patches.

--- 91/91.sh
+++ 91/91.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr91/
-MOZILLA_REV=1278c4a14c3b63acfa63b9d1870e5f2b07f6c246
+MOZILLA_REV=4caf887c1172a3c246c71982cb79c8a3828406cb
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr91/
-COMM_REV=bd3da11e22d0070699026cb5f4e31b44b35580f0
+COMM_REV=458435cfbea94786b16e5d558dca86341356cde4
 RUST_VER=1.53.0
