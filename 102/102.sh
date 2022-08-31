7af9dbea2a42c71f7b3e17c7badaa388d00b1053
Betterbird <betterbird@betterbird.eu>
2022-09-01 00:22:48 +0200

    [102] Prepare for 102.2.1-bb15, fold two patches.

--- 102/102.sh
+++ 102/102.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
 MOZILLA_REV=3876d5327f44c991c9034c4112f33f147ab10ab9
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=ebec14a23058b95d5b8611b50062fa4985a16d41
+COMM_REV=b606061ac37965c6fa7b2f3c1c3d1f849d4f404c
 RUST_VER=1.60.0
