497c37a256caa8271643185dd0e61658e4033a48
Betterbird <betterbird@betterbird.eu>
2022-09-07 22:48:33 +0200

    [102] Use 102.2.2 language packs.

--- 102/scripts/repack.cmd
+++ 102/scripts/repack.cmd
@@ -8,8 +8,7 @@ move omni.ja ..\omni.ja
 del ..\omni.ja
 
 :: Get language pack
-:: wget http://ftp.mozilla.org/pub/thunderbird/releases/91.3.2/linux-x86_64/xpi/%~1.xpi
-wget http://ftp.mozilla.org/pub/thunderbird/candidates/102.2.1-candidates/build1/linux-x86_64/xpi/%~1.xpi
+wget http://ftp.mozilla.org/pub/thunderbird/candidates/102.2.2-candidates/build1/linux-x86_64/xpi/%~1.xpi
 
 :: Remove stuff we don't want from the language pack
 7z d %~1.xpi manifest.json
