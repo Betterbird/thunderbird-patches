82435861382cbba324df91139b48a609f8165408
Betterbird <betterbird@betterbird.eu>
2022-09-09 11:51:01 +0200

    [102] Add script to produce language pack.

---
+++ 102/scripts/langpack.cmd
@@ -0,0 +1,39 @@
+mkdir langpacks
+cd langpacks
+
+:: Get language pack
+wget http://ftp.mozilla.org/pub/thunderbird/candidates/102.2.2-candidates/build1/linux-x86_64/xpi/%~1.xpi
+
+:: Remove stuff we don't want from the language pack
+7z d %~1.xpi chrome\%~1\locale\%~1\branding
+7z d %~1.xpi localization\%~1\branding
+
+:: Unpack the language pack
+7z x %~1.xpi
+rm -f %~1.xpi
+
+:: Fix the manifest.
+sed -i -e 's/langpack-%~1@thunderbird.mozilla.org/langpack-%~1@betterbird.eu/' manifest.json
+sed -i -e 's/Language pack for Thunderbird/Language pack for Betterbird/' manifest.json
+
+:: Copy US branding
+7z x ..\betterbird-%~2.en-US.win64.installer.exe core\omni.ja
+7z x core\omni.ja chrome\en-US\locale\en-US\branding\
+7z x core\omni.ja localization\en-US\branding
+mv chrome\en-US\locale\en-US\branding\ chrome\%~1\locale\%~1\branding\
+mv localization\en-US\branding localization\%~1\branding\
+rmdir /s /q chrome\en-US\locale\en-US
+rmdir /s /q localization\en-US
+
+:: Call script that provides extra strings
+call ..\%~1.cmd
+
+:: Pack the whole thing again
+7z a -r -tzip %~1.xpi manifest.json chrome localization
+
+rmdir /s /q chrome
+rmdir /s /q localization
+rmdir /s /q core
+rm manifest.json
+
+cd ..
