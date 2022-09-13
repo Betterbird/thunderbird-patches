a4e64259ddfdd070e54146c3dcda26d4fb847ebb
Betterbird <betterbird@betterbird.eu>
2022-09-13 20:48:17 +0200

    [91] Switch to official final ESR changeset for 91.13.1-bb37

--- 91/91.sh
+++ 91/91.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr91/
 MOZILLA_REV=4caf887c1172a3c246c71982cb79c8a3828406cb
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr91/
-COMM_REV=0ef73d1be5f720f76aecf61a0ff904c10a520079
+COMM_REV=2f9b465346ceb38b4ab79ccc1bfe8d8ad0bc8743
 RUST_VER=1.53.0
