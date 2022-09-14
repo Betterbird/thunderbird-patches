ec9345823b00d01b782ae82428ee810e802ec7b1
Betterbird <betterbird@betterbird.eu>
2022-09-14 22:42:18 +0200

    [102] Cherry-pick another bug and switch to official ESR changeset.

--- 102/102.sh
+++ 102/102.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
 MOZILLA_REV=c2dd0f1c0adfb1fa1c5d9319fbe023d324c34db6
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=e6467e07745a7d763c52e15ef75e44e121ed4c49
+COMM_REV=ddb36e940acf90b8002ecba61edef43460bab246
 RUST_VER=1.60.0
