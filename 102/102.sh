3b0241e64e420ef5827a00e0e28ec551b753b733
Betterbird <betterbird@betterbird.eu>
2022-09-15 22:37:36 +0200

    [102] Switch to official ESR changeset again.

--- 102/102.sh
+++ 102/102.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
 MOZILLA_REV=c2dd0f1c0adfb1fa1c5d9319fbe023d324c34db6
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=ddb36e940acf90b8002ecba61edef43460bab246
+COMM_REV=04f60827c1b53ab86121c7d122d3a8216219ff9f
 RUST_VER=1.60.0
