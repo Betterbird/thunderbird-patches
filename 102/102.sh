dfd6d974fb7a659c91342a51d75987526c54536a
Betterbird <betterbird@betterbird.eu>
2022-10-05 19:40:18 +0200

    [102] Prepare for 102.3.2-bb19.

--- 102/102.sh
+++ 102/102.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
 MOZILLA_REV=c2dd0f1c0adfb1fa1c5d9319fbe023d324c34db6
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=07a17b101f904a686bbdf798ba2e820079a8323f
+COMM_REV=8d8b229caa5e21c2ff8eaf68a0c14a7247d96818
 RUST_VER=1.60.0
