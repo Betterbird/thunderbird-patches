3a491033cfd7c23bec0f0c9671c74ddc1735fb61
Betterbird <betterbird@betterbird.eu>
2022-10-11 18:25:29 +0200

    Prepare for 102.3.3-bb20, take 2, set comm-esr102 rev.

--- 102/102.sh
+++ 102/102.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
 MOZILLA_REV=c2dd0f1c0adfb1fa1c5d9319fbe023d324c34db6
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=8d8b229caa5e21c2ff8eaf68a0c14a7247d96818
+COMM_REV=afeec21c1fcc27ba58f98f629e85609a728f79e6
 RUST_VER=1.60.0
