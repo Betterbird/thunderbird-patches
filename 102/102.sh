ab4a9b57cf924694da75463c9a9c5a7551180796
Betterbird <betterbird@betterbird.eu>
2022-10-25 22:04:56 +0200

    Prepare for 102.4.1-bb22. Use official fix for bug 1794997.

--- 102/102.sh
+++ 102/102.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
 MOZILLA_REV=52928ba6fd91414091326fe6339bc63e89b593a6
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=c064680366a130c4c02ba9b85ef324f0f075c47b
+COMM_REV=e572bc3cfa07492189aec439e98378b0811ae3bb
 RUST_VER=1.60.0
