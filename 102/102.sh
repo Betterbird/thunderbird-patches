5afcd31ffa6ed6df97a4e1eaa4e3b0418753cbec
Betterbird <betterbird@betterbird.eu>
2022-11-02 21:52:43 +0100

    Switch 102.4.2-bb23 to build2.

--- 102/102.sh
+++ 102/102.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
 MOZILLA_REV=52928ba6fd91414091326fe6339bc63e89b593a6
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=7b0bbd650be4b8cbccafe41df4fb0011b1ab3c52
+COMM_REV=bece6c033f6b24b9c126598da7c6eb5bc2a48b14
 RUST_VER=1.60.0
