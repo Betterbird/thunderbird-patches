78ea7fb591c3fda3f50884c8d1db2262d8d0352b
Betterbird <betterbird@betterbird.eu>
2022-12-12 22:38:06 +0100

    Switch to official changeset and merge end-of-year donation appeal patches (result: use BB's own URL).

--- 102/102.sh
+++ 102/102.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
 MOZILLA_REV=72b501d7159eb634a961e27157a3af58360a0b74
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=563cc2baf242975fda41000da903db513713dc65
+COMM_REV=d46a947ae837300f19cc45c75d473c7b1cf4ebb3
 RUST_VER=1.60.0
