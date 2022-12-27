c33c762a1438d9bbdd0c6397cfa45395a46063a5
Besnik Bleta <besnik@programeshqip.org>
2022-12-27 23:21:40 +0200

    Adding sq locale (#110)

--- 102/scripts/sq.cmd
+++ 102/scripts/sq.cmd
@@ -48,7 +48,7 @@ set lecho=C:\mozilla-build\msys\bin\lessecho.exe
 %lecho% "mail-multiline-individual-folders ="                                 >> localization\sq\messenger\menubar.ftl
 %lecho% "    .label = Shfaqje Me Shumë Rreshta Përmes Shtyllës Së Subjekteve" >> localization\sq\messenger\menubar.ftl
 %lecho% "mail-multiline-no-folders ="                                         >> localization\sq\messenger\menubar.ftl
-%lecho% "    .label = Sans affichage multiligne"                              >> localization\sq\messenger\menubar.ftl
+%lecho% "    .label = Pa Shfaqje Me Shumë Rreshta"                            >> localization\sq\messenger\menubar.ftl
 
 :: Original strings:
 :: compose-send-plain-or-html-option =
@@ -65,12 +65,12 @@ set lecho=C:\mozilla-build\msys\bin\lessecho.exe
 
 :: Movemail
 :: Strings from https://hg.mozilla.org/l10n-central/sq/file/1a8b669b9479e2c09a730537d6d10dd400070a33/mail/chrome/messenger/localMsgs.properties
-%lecho% "movemailCantOpenSpoolFile=S’arrihet të hapet kartelë \"mail spool\" %S."   >> chrome\sq\locale\sq\messenger\localMsgs.properties
-%lecho% "movemailCantCreateLock=S’arrihet të krijohet kartelë kyçjeje %S. Që të mund të punojë \"movemail\", lypset të krijohen kartela kyçjesh në drejtorinë \"mail spool\". Në mjaft sisteme, kjo arrihet më mirë duke e bërë drejtorinë \"spool\" të jetë nën mënyrën 01777." >> chrome\sq\locale\sq\messenger\localMsgs.properties
+%lecho% "movemailCantOpenSpoolFile=S’arrihet të hapet kartelë “mail spool” %S."   >> chrome\sq\locale\sq\messenger\localMsgs.properties
+%lecho% "movemailCantCreateLock=S’arrihet të krijohet kartelë kyçjeje %S. Që të mund të punojë “movemail”, lypset të krijohen kartela kyçjesh në drejtorinë “mail spool”. Në mjaft sisteme, kjo arrihet më mirë duke e bërë drejtorinë “mail spool” të jetë nën lejet 01777." >> chrome\sq\locale\sq\messenger\localMsgs.properties
 %lecho% "movemailCantDeleteLock=S’arrihet të fshihet kartelë kyçjeje %S."           >> chrome\sq\locale\sq\messenger\localMsgs.properties
-%lecho% "movemailCantTruncateSpoolFile=S’arrihet të cungohet kartelë \"spool\" %S." >> chrome\sq\locale\sq\messenger\localMsgs.properties
-%lecho% "movemailSpoolFileNotFound=S’arrihet të gjendet kartelë \"mail spool\"."    >> chrome\sq\locale\sq\messenger\localMsgs.properties
-%lecho% "movemailCantParseSpool=S’arrihet të përtypet kartelën spool %S. Kartela mund të jetë e dëmtuar ose e pavlefshme." >> chrome\sq\locale\sq\messenger\localMsgs.properties
+%lecho% "movemailCantTruncateSpoolFile=S’arrihet të cungohet kartelë “mail spool” %S." >> chrome\sq\locale\sq\messenger\localMsgs.properties
+%lecho% "movemailSpoolFileNotFound=S’arrihet të gjendet kartelë “mail spool”."    >> chrome\sq\locale\sq\messenger\localMsgs.properties
+%lecho% "movemailCantParseSpool=S’arrihet të përtypet kartelë “mail spool” %S. Kartela mund të jetë e dëmtuar ose e pavlefshme." >> chrome\sq\locale\sq\messenger\localMsgs.properties
 %lecho% "serverType-movemail=Unix Movemail"                                         >> chrome\sq\locale\sq\messenger\messenger.properties
 
 %lecho% "account-action-add-movemail-account ="     >> localization\sq\messenger\accountManager.ftl
@@ -79,3 +79,4 @@ set lecho=C:\mozilla-build\msys\bin\lessecho.exe
 %lecho% "file-new-movemail-account ="               >> localization\sq\messenger\menubar.ftl
 %lecho% "    .label = Llogari Movemail…"            >> localization\sq\messenger\menubar.ftl
 %lecho% "    .accesskey = M"                        >> localization\sq\messenger\menubar.ftl
+
