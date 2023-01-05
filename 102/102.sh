9684211880fd705d35b8f3dfe45da01853302871
Betterbird <betterbird@betterbird.eu>
2023-01-05 19:50:52 +0100

    Prepare for 102.7.0-bb28, remove one hunk from patch for bug 1554188 landed in bug 1792071.

--- 102/102.sh
+++ 102/102.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
-MOZILLA_REV=72b501d7159eb634a961e27157a3af58360a0b74
+MOZILLA_REV=7968f9296e4c4f49a6efd1d9ccfaec9fe4f9b5b7
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=9552cbbea10b3734ba202ada68e764abe4493b2a
+COMM_REV=d821b5622d80d9bc40f93e77228bc94f1a9efb7a
 RUST_VER=1.60.0
