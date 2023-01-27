e5dd5e94a47b0d7210b9f820f62bcb63496ae433
Betterbird <betterbird@betterbird.eu>
2023-01-27 22:48:56 +0100

    Issue #115: Improve launcher: 1) Activate when already running on same profile/path 2) Pass command arguments to cater for mailto: URLs.

--- BetterbirdLauncher/BetterbirdLauncher/BetterbirdLauncher.cpp
+++ BetterbirdLauncher/BetterbirdLauncher/BetterbirdLauncher.cpp
@@ -3,9 +3,84 @@
 #include <objbase.h>
 #include <windows.h>
 #include <stdio.h>
-#define MAX_PATH_PROFILE MAX_PATH + 50
+#include <tlhelp32.h>
+#define MAX_PATH_PROFILE MAX_PATH + 1000
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+#define DOPRINT 0
+
+#if DOPRINT
+char msg[MAX_PATH + 100];
+FILE* f = NULL;
+#endif
+
+bool checkInfo(PROCESSENTRY32& processInfo, TCHAR *appPath) {
+  if (_wcsicmp(processInfo.szExeFile, L"betterbird.exe") == 0) {
+    MODULEENTRY32 entry;
+    entry.dwSize = sizeof(MODULEENTRY32);
+    HANDLE hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, processInfo.th32ProcessID); 
+    Module32First(hModuleSnap, &entry);
+#if DOPRINT
+    sprintf_s(msg, sizeof(msg), "BetterBirdRunningOnOtherPath: Process  %S\n", entry.szExePath);
+    fprintf(f, msg);
+#endif
+    if (wcslen(appPath) != (wcslen(entry.szExePath) - strlen("\\core\\betterbird.exe")) ||
+        wcsncmp(entry.szExePath, appPath, wcslen(appPath)) != 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool BetterBirdRunningOnOtherPath(TCHAR *appPath) {
+#if DOPRINT
+  fopen_s(&f, "D:\\Desktop\\launcher.txt", "a");
+  sprintf_s(msg, sizeof(msg), "BetterBirdRunningOnOtherPath: Launcher %S\n", appPath);
+  fprintf(f, msg);
+#endif
+
+  PROCESSENTRY32 processInfo;
+  processInfo.dwSize = sizeof(processInfo);
+  HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+  if (processesSnapshot == INVALID_HANDLE_VALUE) {
+#if DOPRINT
+    fclose(f);
+#endif
+    return false;
+  }
+
+  bool found;
+  Process32First(processesSnapshot, &processInfo);
+  found = checkInfo(processInfo, appPath);
+  if (found) {
+    CloseHandle(processesSnapshot);
+#if DOPRINT
+    fprintf(f, "Found instance on different path\n");
+    fclose(f);
+#endif
+    return true;
+  }
+
+  while (Process32Next(processesSnapshot, &processInfo)) {
+    found = checkInfo(processInfo, appPath);
+    if (found) {
+      CloseHandle(processesSnapshot);
+#if DOPRINT
+      fprintf(f, "Found instance on different path\n");
+      fclose(f);
+#endif
+      return true;
+    }
+  }
+
+  CloseHandle(processesSnapshot);
+#if DOPRINT
+  fprintf(f, "Found NO instance on different path\n");
+  fclose(f);
+#endif
+  return false;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow) {
   TCHAR appPath[MAX_PATH];
   TCHAR cacheFile[MAX_PATH];
   TCHAR profilePath[MAX_PATH_PROFILE];
@@ -17,19 +92,28 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
   *lastSlash = 0;
 
   // Prepare profile folder path.
-  wcscpy_s(profilePath, L"-new-instance -profile \"");
+  if (BetterBirdRunningOnOtherPath(appPath)) {
+    wcscpy_s(profilePath, L"-new-instance -profile \"");
+  } else {
+    wcscpy_s(profilePath, L"-profile \"");
+  }
+
   wcscat_s(profilePath, MAX_PATH_PROFILE, appPath);
   wcscat_s(profilePath, MAX_PATH_PROFILE, L"\\profile\"");
+  // Hand on the command line for opening mailto: URLs.
+  wcscat_s(profilePath, MAX_PATH_PROFILE, L" ");
+  wcscat_s(profilePath, MAX_PATH_PROFILE, lpCmdLine);
 
+#if 0
   // Path to the cache file we want to delete.
   wcscpy_s(cacheFile, appPath);
   wcscat_s(cacheFile, MAX_PATH_PROFILE, L"\\profile\\addonStartup.json.lz4");
   bool res = DeleteFileW(cacheFile);
-#if 0
+#if DOPRINT
   DWORD err = GetLastError();
-  char msg[MAX_PATH + 100];
   sprintf_s(msg, sizeof(msg), "BetterbirdLauncher: Deleted %S with success %d %lx\n", cacheFile, res, err);
-  OutputDebugStringA(msg);
+  fprintf(f, msg);
+#endif
 #endif
 
   // Add executable.
