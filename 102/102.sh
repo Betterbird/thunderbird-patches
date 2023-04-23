4e55c52dfbbb4a6878c142f2f969990496e1cf2f
Betterbird <betterbird@betterbird.eu>
2023-04-23 13:55:14 +0200

    Prepare for 102.10.1-bb34. Reshuffle series file.

--- 102/102.sh
+++ 102/102.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
 MOZILLA_REV=737a5c36e0f939b688ff1d6fb75b139cfdf60ae9
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=d8df3bebc4b529388b62b9cb4df152f13910fbe3
+COMM_REV=6eabe0915354d878fe93e2b02547cebe83f1fd6d
 RUST_VER=1.60.0
