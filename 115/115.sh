ea71ea346cfb44f54b6b2c6b9c520434a7fec58d
Betterbird <betterbird@betterbird.eu>
2023-05-07 10:05:58 +0200

    [115] Rebase to latest m-c/c-c, record changesets in 115.sh.

---
+++ 115/115.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
+MOZILLA_REV=ca770a49d132884d09eacf5ff496e2c33e1a889b
+COMM_REPO=https://hg.mozilla.org/comm-central
+COMM_REV=c6e6ae97c1b2e132045835432f656c469b457d22
+RUST_VER=1.67.0
