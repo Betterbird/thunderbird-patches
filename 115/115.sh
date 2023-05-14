dcbdae9e59a55a667e48a057433a5a9deea8833d
Betterbird <betterbird@betterbird.eu>
2023-05-14 15:30:10 +0200

    [115] Pick up more changesets.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
 MOZILLA_REV=896ce1fb1f9ca2333a46dde452ed9328eb94a649
 COMM_REPO=https://hg.mozilla.org/comm-central
-COMM_REV=4afd838d6921bd4f42c6f277521fb81b5b2401f8
+COMM_REV=81fcadefc32b3d9478c1ff6f2ec236dd814af897
 RUST_VER=1.67.0
