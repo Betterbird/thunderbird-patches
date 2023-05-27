8607d948ac9a1412ce7b6802af208a2cae14d1e5
Betterbird <betterbird@betterbird.eu>
2023-05-27 12:07:40 +0200

    [115] Rebased to latest C-C version 115.

--- 115/115.sh
+++ 115/115.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
-MOZILLA_REV=eaba732ce0aba94186e6e881a23502053168cfdf
+MOZILLA_REV=fc6056442a0fa16146259cb730d4e34a16656952
 COMM_REPO=https://hg.mozilla.org/comm-central
-COMM_REV=3630ecac71b0a4171cb1bcd85fd3fb2e07bd5880
+COMM_REV=60acd5aa9b4abb4929d0b943529fa6de92cf0624
 RUST_VER=1.67.0
