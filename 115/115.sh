a13baf01f602e613856a74c60d3150f8dfa8ec83
Betterbird <betterbird@betterbird.eu>
2023-06-12 21:24:07 +0200

    [115] Rebased to 115.0b2 and added a bunch of patches TB should have taken.

--- 115/115.sh
+++ 115/115.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
-MOZILLA_REV=8b6f8763d212b11acd7421760ab63612ce65bda4
+MOZILLA_REV=fb3ad1dbd6bbf62e33596cc17abc8bff6a4f425f
 COMM_REPO=https://hg.mozilla.org/releases/comm-beta
-COMM_REV=58b577d2d324e21d6e71aef54b4d7e1191b2db4d
+COMM_REV=3e51d119baf16344e2c54ca99fd82be384b799f4
 RUST_VER=1.67.0
