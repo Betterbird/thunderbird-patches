7863b229a3ebd3885eda9afaf6e8e5f90802f861
Betterbird <betterbird@betterbird.eu>
2023-06-14 10:03:57 +0200

    Adjust to TB's latest build of TB 115.0b2.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
 MOZILLA_REV=fb3ad1dbd6bbf62e33596cc17abc8bff6a4f425f
 COMM_REPO=https://hg.mozilla.org/releases/comm-beta
-COMM_REV=3e51d119baf16344e2c54ca99fd82be384b799f4
+COMM_REV=1ca352eb2a1067237c4106e69dc1ee781147d5c7
 RUST_VER=1.67.0
