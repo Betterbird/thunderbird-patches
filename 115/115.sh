b00da93a5c17be08d501b0e3ba51d61a22bcd7ce
Betterbird <betterbird@betterbird.eu>
2023-06-26 22:47:48 +0200

    [115] Rebased to 115.0b5: rebased four patches.

--- 115/115.sh
+++ 115/115.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
-MOZILLA_REV=d30b3cd164afe66b9db586154a0af4b5d4791308
+MOZILLA_REV=f539365f51c8542b1e354b68e50907668f16878d
 COMM_REPO=https://hg.mozilla.org/releases/comm-beta
-COMM_REV=16cade705109d982f14495ba09a996c76c2d072f
+COMM_REV=7b74680c9b00bd79607c408881c6579ebac2399a
 RUST_VER=1.67.0
