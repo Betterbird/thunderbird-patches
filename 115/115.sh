340e652de58a353fd128e6bfdffff922370b8b02
Betterbird <betterbird@betterbird.eu>
2023-06-27 09:26:38 +0200

    [115] Rebased to 115.0b5, take 2, cherry picked some patches.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
 MOZILLA_REV=f539365f51c8542b1e354b68e50907668f16878d
 COMM_REPO=https://hg.mozilla.org/releases/comm-beta
-COMM_REV=7b74680c9b00bd79607c408881c6579ebac2399a
+COMM_REV=fe2c8801535c0addf1c366b25f37e792feb0d4c3
 RUST_VER=1.67.0
