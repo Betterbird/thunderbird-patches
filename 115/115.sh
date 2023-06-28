5cfa915708510f64f4f8578fbc4a6c2066dff788
Betterbird <betterbird@betterbird.eu>
2023-06-28 21:26:33 +0200

    [115] Rebased to 115.0b6.

--- 115/115.sh
+++ 115/115.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
-MOZILLA_REV=f539365f51c8542b1e354b68e50907668f16878d
+MOZILLA_REV=035b72e275569245ab5c8844f4ddca8f5ce7da42
 COMM_REPO=https://hg.mozilla.org/releases/comm-beta
-COMM_REV=fe2c8801535c0addf1c366b25f37e792feb0d4c3
+COMM_REV=a95367d1332adb9b5ace0ea4e595be28ef9ac963
 RUST_VER=1.67.0
