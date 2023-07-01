a36bba2471841a6417ae9a0253c367e40b317b12
Betterbird <betterbird@betterbird.eu>
2023-07-01 13:12:06 +0200

    [102] Prepare for 102.13.0-bb38.

--- 102/102.sh
+++ 102/102.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
-MOZILLA_REV=436a2ee5bfc7c4e70c0d6bc14bf1946e66c1a231
+MOZILLA_REV=3ba84f15ffa6092b5587de58f09dbc4240bcf8e2
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=9d42734e12597ccdb59fee178bf369d8c328dcad
+COMM_REV=6bac487a2e0fe43da2e1f611416554e1dab641c2
 RUST_VER=1.60.0
