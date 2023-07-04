6386de0c3e6025fdd6484526dc4ef19b5ae2b0e0
Betterbird <betterbird@betterbird.eu>
2023-07-04 22:57:54 +0200

    [115] Rust 1.70.0 is retrieved on "mach bootstrap" for 115, so let's go with that version.

--- 115/115.sh
+++ 115/115.sh
@@ -3,4 +3,4 @@ MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=4a8666238269bba3dcccc95812d13d2c8abd9252
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
 COMM_REV=edb5fa7b3359b01d3014455083bc8e458e0c7fa8
-RUST_VER=1.67.0
+RUST_VER=1.70.0
