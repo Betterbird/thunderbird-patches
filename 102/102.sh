2460fcc8db4a0802eb9974528d53a216e614ba73
Betterbird <betterbird@betterbird.eu>
2023-07-04 20:37:38 +0200

    [102] Prepare for 102.13.0-bb38, take 2: Rebase to TB's latest official revision.

--- 102/102.sh
+++ 102/102.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
 MOZILLA_REV=3ba84f15ffa6092b5587de58f09dbc4240bcf8e2
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=6bac487a2e0fe43da2e1f611416554e1dab641c2
+COMM_REV=2bf94c4d195694485df5d632f2453888cf4f6657
 RUST_VER=1.60.0
