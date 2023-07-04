4b6b722d8f59a1801f6f3a4a5445c1df610e9143
Betterbird <betterbird@betterbird.eu>
2023-07-04 20:51:53 +0200

    [115] Switch to build from mozilla-esr115 and comm-esr115.

--- 115/115.sh
+++ 115/115.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
-MOZILLA_REV=035b72e275569245ab5c8844f4ddca8f5ce7da42
-COMM_REPO=https://hg.mozilla.org/releases/comm-beta
-COMM_REV=a95367d1332adb9b5ace0ea4e595be28ef9ac963
+MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
+MOZILLA_REV=4a8666238269bba3dcccc95812d13d2c8abd9252
+COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
+COMM_REV=edb5fa7b3359b01d3014455083bc8e458e0c7fa8
 RUST_VER=1.67.0
