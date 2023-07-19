e9acb2463a3e50fe5f07f7b3d5d145e159b8fca9
Betterbird <betterbird@betterbird.eu>
2023-07-19 20:04:04 +0200

    [115] Prepare for 115.0.1-bb7, take 3.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=872b5aae170ffa755f89d0c6a9ebc380af677e9a
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=b613d3149763dd2ec84383784a8a2d18b00ec746
+COMM_REV=d0fb46f5902aa115c678af4e1cc50ec5782cf67c
 RUST_VER=1.70.0
