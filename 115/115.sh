83c9c8dff687c0b8a6c37ce7d3655bc9682076d4
Betterbird <betterbird@betterbird.eu>
2023-07-31 20:53:28 +0200

    [115] Prepare for 115.1.0-bb8, rebase one patch, fold one patch, take 2. (Let's see if/when the TB people notice that they missed a bug.)

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=662ab9287f0197d9799c6c15fde1ec07bfce8865
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=cb69a560bb71f3ad8cd1c8fb2f06ad5e0f75c9f0
+COMM_REV=bbb351cd8954e8aff602a61e4d79ab63e41d5643
 RUST_VER=1.70.0
