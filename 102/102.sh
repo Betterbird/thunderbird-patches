a61ee1f7a3ae6b2877f902085f762a3bceb84faa
Betterbird <betterbird@betterbird.eu>
2023-08-01 20:03:21 +0200

    [102] Prepare for 102.14.0-bb39, rebase one patch.

--- 102/102.sh
+++ 102/102.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
-MOZILLA_REV=3ba84f15ffa6092b5587de58f09dbc4240bcf8e2
+MOZILLA_REV=381b375f07554410a27f95cefbc76b74c8274b46
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=2bf94c4d195694485df5d632f2453888cf4f6657
+COMM_REV=d83d81499d87f2360995ae0448fcaa40e0cd106a
 RUST_VER=1.60.0
