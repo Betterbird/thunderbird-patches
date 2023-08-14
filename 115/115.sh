04f1ddefa684e44952c54b33851c96865c6653c8
Betterbird <betterbird@betterbird.eu>
2023-08-14 21:30:23 +0200

    [115] Switch to official final changeset for 115.1.1 build 2. Rebase one patch. Move TB patches down in series file. Cherry pick another bug.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=662ab9287f0197d9799c6c15fde1ec07bfce8865
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=86b12f2bfdc296680a4ffa895a29d946c23d1098
+COMM_REV=a61b0f49985f18cc9f04818fd5120c19cf66a7f2
 RUST_VER=1.70.0
