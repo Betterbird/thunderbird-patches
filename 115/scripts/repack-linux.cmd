85c4d84dc06f26374aecf131eae185d77f6c8b68
Betterbird <betterbird@betterbird.eu>
2023-08-28 23:08:50 +0200

    [115] Switch from 115.1.1 to 115.2.0 in repack scripts and metadata.

--- 115/scripts/repack-linux.cmd
+++ 115/scripts/repack-linux.cmd
@@ -10,7 +10,7 @@ move omni.ja ..\omni.ja
 del ..\omni.ja
 
 :: Get language pack
-wget http://ftp.mozilla.org/pub/thunderbird/candidates/115.1.1-candidates/build2/linux-x86_64/xpi/%~1.xpi
+wget http://ftp.mozilla.org/pub/thunderbird/candidates/115.2.0-candidates/build2/linux-x86_64/xpi/%~1.xpi
 
 :: Remove stuff we don't want from the language pack
 7z d %~1.xpi manifest.json
