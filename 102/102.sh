93e9a14b49470580019519ae20deaf3e88da65e2
Betterbird <betterbird@betterbird.eu>
2023-08-29 00:10:21 +0200

    [102] Prepare for 102.15.0-bb40, pick three patches from 115, including minimise to tray on Linux.

--- 102/102.sh
+++ 102/102.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr102/
-MOZILLA_REV=381b375f07554410a27f95cefbc76b74c8274b46
+MOZILLA_REV=6db0d7b1b1653e0bbe3058e373235c673205adb4
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr102/
-COMM_REV=d83d81499d87f2360995ae0448fcaa40e0cd106a
+COMM_REV=ac0b189e3bc07ee798d5ed84e99f874bc72204a6
 RUST_VER=1.60.0
