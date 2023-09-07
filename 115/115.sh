f0b9345c7c4c1c0b6ac494843b2588fd2b5fc347
Betterbird <betterbird@betterbird.eu>
2023-09-07 21:43:29 +0200

    [115] Prepare for 115.2.1-bb11: Rebase one patch, cherry pick two bugs.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=bf2f656d025836975b7bf6aeb16901a037a01923
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=ffd49506d62c329824f7adb4896602a8c77ec7e8
+COMM_REV=4eceb4c02f9baee085631e6308f60588ce2fbafe
 RUST_VER=1.70.0
