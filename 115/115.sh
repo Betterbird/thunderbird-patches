de662aac85c23ced537efe5f9b14b15a4c618921
Betterbird <betterbird@betterbird.eu>
2023-09-15 22:49:20 +0200

    [115] Prepare for 115.2.3-bb12: Added patch to back out bug 1849000: Default to card view and vertical view.

--- 115/115.sh
+++ 115/115.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
-MOZILLA_REV=bf2f656d025836975b7bf6aeb16901a037a01923
+MOZILLA_REV=2e61ec742c2039eaacac6c4d99c919db51a34b68
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=4eceb4c02f9baee085631e6308f60588ce2fbafe
+COMM_REV=b1513addaffe7ff402c8b3ffaaf340e4fb980c38
 RUST_VER=1.70.0
