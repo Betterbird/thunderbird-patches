8667720bf2fa9a3ef6cc45041c0ce64ef585ac57
Betterbird <betterbird@betterbird.eu>
2023-09-26 00:08:52 +0200

    Prepare for 115.3.0-bb13.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=17bedcda2f5c28b7fbc136d300bdbbf04f382e00
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=637b9308f926ec203608ab5e5da0de39311ec0c9
+COMM_REV=4afebff8f52461f9f74aa6d8c2d98e90c408c4a4
 RUST_VER=1.70.0
