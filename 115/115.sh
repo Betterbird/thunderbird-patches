7e7706b5aad4088b034a7fc838f65b24a0ff20be
Betterbird <betterbird@betterbird.eu>
2023-09-26 20:09:45 +0200

    [115] Prepare for 115.3.0-bb13, take 2: Official changeset, cherry-picked another bug, removed unneeded patch.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=17bedcda2f5c28b7fbc136d300bdbbf04f382e00
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=4afebff8f52461f9f74aa6d8c2d98e90c408c4a4
+COMM_REV=1218aebde8d83fe28bbdfe39c63d7a10044829c2
 RUST_VER=1.70.0
