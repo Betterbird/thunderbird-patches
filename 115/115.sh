d380e2a7d759bb2fbfbc8897aa7be0b6c155ca5b
Betterbird <betterbird@betterbird.eu>
2023-10-09 20:24:15 +0200

    Prepare for 115.3.2-bb15: Merge 02*-branding patches, rebase three patches.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=749617c4473c65f8aebb2e3254777ae99dc17cce
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=42b3addbca70f64493bdbede7f3f66dc40641a0a
+COMM_REV=85cf271c4bf74ce9a32b2b3cf7e19a820b8a3fe6
 RUST_VER=1.70.0
