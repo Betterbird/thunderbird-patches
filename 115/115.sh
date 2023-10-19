02a74a52946db61ed2b9c7c6bdc4347b48c2e687
Betterbird <betterbird@betterbird.eu>
2023-10-19 23:46:21 +0200

    Prepare for 115.4.0-bb16, take 3: Set official revision in 115.sh.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=7e770707fe156fc2e62c5f624235e4b2dadc1e7b
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=c8c26514b496872c979686a31a916f36c0b94313
+COMM_REV=e579b5ead616a53f399e6a0d18bfee5498bbdb39
 RUST_VER=1.70.0
