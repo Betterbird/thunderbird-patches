b3afe683d65253c9f907b6e1e9af7df479082f24
Betterbird <betterbird@betterbird.eu>
2023-11-04 00:04:39 +0100

    Prepare for 115.4.2-bb17: rebase 5 patches

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=7e770707fe156fc2e62c5f624235e4b2dadc1e7b
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=e20d8f7dfd45a6a8f7acff356fe45425f1d6b52b
+COMM_REV=c114151b0b64f24b201a7642e493460f570885a2
 RUST_VER=1.70.0
