272ecff4a1df047c6e5aa219effc7d64a0a209a0
Betterbird <betterbird@betterbird.eu>
2023-11-04 21:42:32 +0100

    Prepare for 115.4.2-bb17, take 2: Switch to official changeset, shuffle cherry-picked bugs down, pick one more, rebase one patch.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=7e770707fe156fc2e62c5f624235e4b2dadc1e7b
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=c114151b0b64f24b201a7642e493460f570885a2
+COMM_REV=713f0c8db2933f87d523d36e25fa733bea6694b3
 RUST_VER=1.70.0
