044a0bcb0d61d905b126ad71488d781390a98aaf
Betterbird <betterbird@betterbird.eu>
2023-11-26 16:48:30 +0100

    Prepare for 115.5.1-bb19: Rebase two patches.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=8a02a7c43f1eddfd18926f0266d188b4f359c0aa
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=c4784aa6a6d6f84f8745761d554effca3df20e34
+COMM_REV=070cf9cc26acb6accc2d88a8118249a08c274c7e
 RUST_VER=1.70.0
