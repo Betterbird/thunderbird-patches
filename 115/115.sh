8ef9e9b3282f35efe4908a378ff5f586d12032cf
Betterbird <betterbird@betterbird.eu>
2023-12-08 17:14:03 +0100

    Prepare for 115.5.2-bb20, take 3: Switch to official changeset, reinstate backed-out bug 1864338 and cherry-pick 1868974, rebased one patch.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=8a02a7c43f1eddfd18926f0266d188b4f359c0aa
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=b5109afe6fba610c72839713b700b10dc15c806e
+COMM_REV=a8cffa1042782293f03a22eebadf4fe01b566bac
 RUST_VER=1.70.0
