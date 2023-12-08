e15e279cd7c6974daea4173e32eed5e66c3ccb42
Betterbird <betterbird@betterbird.eu>
2023-12-08 13:41:36 +0100

    Prepare for 115.5.2-bb20, take 2: Rebase 6 patches and switch to official changeset.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=8a02a7c43f1eddfd18926f0266d188b4f359c0aa
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=070cf9cc26acb6accc2d88a8118249a08c274c7e
+COMM_REV=b5109afe6fba610c72839713b700b10dc15c806e
 RUST_VER=1.70.0
