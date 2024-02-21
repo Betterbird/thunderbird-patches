c999eb48bd128d4ab89f5e76e8835c9656e4146b
Betterbird <betterbird@betterbird.eu>
2024-02-21 11:26:19 +0100

    Switch to official final changeset for 115.8.0 build 1. Rebase one patch. Cherry pick two more bugs.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=17fcbdf6426663c10da5d0ba21927e71184304c3
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=536ce49d7038061865e7baa28813becf38b0fb72
+COMM_REV=7b8fc4b9edc8825b4f16238a3715c0f785283b1c
 RUST_VER=1.70.0
