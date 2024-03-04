072f1d78a47d270a5c4f37d8e7c18c2c541c32f4
Betterbird <betterbird@betterbird.eu>
2024-03-04 16:36:43 +0100

    Prepare for 115.8.1-bb25: Remove 10 patches upstream has now included.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=17fcbdf6426663c10da5d0ba21927e71184304c3
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=7b8fc4b9edc8825b4f16238a3715c0f785283b1c
+COMM_REV=4e1309331edd0e40ed622c14cef07eb5a3bc67eb
 RUST_VER=1.70.0
