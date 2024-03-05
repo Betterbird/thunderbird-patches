9079287a549cf976914c373b0873b5818b5a0680
Betterbird <betterbird@betterbird.eu>
2024-03-05 11:34:51 +0100

    Prepare for 115.8.1-bb25, take 3. Switch to build2 changeset and remove a patch that the TB folks are now including.

--- 115/115.sh
+++ 115/115.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=17fcbdf6426663c10da5d0ba21927e71184304c3
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=e182f51197097fcaba23d29bad6dd67413932e71
+COMM_REV=5b8708dc0314dab535d9357b5dfd6b2bef9441c8
 RUST_VER=1.70.0
