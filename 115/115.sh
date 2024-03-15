07f7a55df29331d7ceb9383910816fab06702a9d
Betterbird <betterbird@betterbird.eu>
2024-03-15 21:21:43 +0100

    Prepare for 115.9.0-bb26, take 1: Remove 8 patches upstream has now included, rebase 2 patches.

--- 115/115.sh
+++ 115/115.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
-MOZILLA_REV=17fcbdf6426663c10da5d0ba21927e71184304c3
+MOZILLA_REV=f3dd0afed51cb7283d4fa06a0c40286eee657e30
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=5b8708dc0314dab535d9357b5dfd6b2bef9441c8
+COMM_REV=1e95a096fd3c6053c4eeeca935eb8227cf9ee25c
 RUST_VER=1.70.0
