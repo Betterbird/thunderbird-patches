f1137665a44c899e90db49cc0c0e8d1d08a4f753
Betterbird <betterbird@betterbird.eu>
2024-03-28 11:06:40 +0100

    [128] Rebased 4 patches, updated 1, added 128.sh.

---
+++ 128/128.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
+MOZILLA_REV=f8ac03b3c764017d5704deed413c8d35c99978fd
+COMM_REPO=https://hg.mozilla.org/comm-central/
+COMM_REV=1e95a096fd3c6053c4eeeca935eb8227cf9ee25c
+RUST_VER=1.76.0
