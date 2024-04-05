928efe0f4e3ad68cf6e671b35ce6ae7434065aa9
Betterbird <betterbird@betterbird.eu>
2024-04-05 20:13:20 +0200

    [128] Rebase to C-C rev. a9937a0b9308fb4572887559fe9414e33b46e4fb: Rebase 3 patches, drop 1.

--- 128/128.sh
+++ 128/128.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
 MOZILLA_REV=f8ac03b3c764017d5704deed413c8d35c99978fd
 COMM_REPO=https://hg.mozilla.org/comm-central/
-COMM_REV=1e95a096fd3c6053c4eeeca935eb8227cf9ee25c
+COMM_REV=a9937a0b9308fb4572887559fe9414e33b46e4fb
 RUST_VER=1.76.0
