5dfe6d3533dabe8385db6aef718647c1489d8409
Betterbird <betterbird@betterbird.eu>
2024-04-12 00:32:22 +0200

    Prepare for 115.10.0-bb27: Remove 5 patches upstream has now included, rebase 9 patches.

--- 115/115.sh
+++ 115/115.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
-MOZILLA_REV=f3dd0afed51cb7283d4fa06a0c40286eee657e30
+MOZILLA_REV=64fc0d6e7ff6b1d626e40d45c492fa210591a1c0
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=1e95a096fd3c6053c4eeeca935eb8227cf9ee25c
+COMM_REV=6b83c6e7c769f89ab59d1d589a0e3d2a15011467
 RUST_VER=1.70.0
