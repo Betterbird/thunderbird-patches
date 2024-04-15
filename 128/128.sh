819f56709c16ca7d681235fa944ba032c8747a29
Betterbird <betterbird@betterbird.eu>
2024-04-16 00:12:43 +0200

    [128] Also set last changeset of version 126 for M-C.

--- 128/128.sh
+++ 128/128.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
-MOZILLA_REV=f8ac03b3c764017d5704deed413c8d35c99978fd
+MOZILLA_REV=25a22c2f939167a119d912b3fe61312db879d576
 COMM_REPO=https://hg.mozilla.org/comm-central/
 COMM_REV=7435a60e1c7c60fa4e871f408f8d02a525176220
 RUST_VER=1.76.0
