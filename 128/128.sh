f3f58cf13dc0869f1a44d638dc8ec546b5c73280
Betterbird <betterbird@betterbird.eu>
2024-04-15 21:21:03 +0200

    Rebase 9 patches to last changeset of version 126.

--- 128/128.sh
+++ 128/128.sh
@@ -2,5 +2,5 @@
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
 MOZILLA_REV=f8ac03b3c764017d5704deed413c8d35c99978fd
 COMM_REPO=https://hg.mozilla.org/comm-central/
-COMM_REV=a9937a0b9308fb4572887559fe9414e33b46e4fb
+COMM_REV=7435a60e1c7c60fa4e871f408f8d02a525176220
 RUST_VER=1.76.0
