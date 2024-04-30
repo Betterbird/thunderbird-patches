960ac1b03785f1b8fe17811f15714aa404be8059
Betterbird <betterbird@betterbird.eu>
2024-04-30 21:34:12 +0200

    [128] Rebase 4 patches to current M-C/C-C changesets at version 127, drop account provisioner patch.

--- 128/128.sh
+++ 128/128.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
-MOZILLA_REV=3de3f9428f138bf269718fceb36c5112a195ca37
+MOZILLA_REV=5502377f2629549b2b7c5368c980d0487ed281d4
 COMM_REPO=https://hg.mozilla.org/comm-central/
-COMM_REV=99d6072c892bb217745168985e6c2808bcd8f677
+COMM_REV=5e9b6b0fede8fb6a061e976c66cbef79d411242a
 RUST_VER=1.76.0
