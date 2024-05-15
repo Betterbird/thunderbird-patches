9814c1ced57c4285da37678d641d988ede9cd6ce
Betterbird <betterbird@betterbird.eu>
2024-05-15 11:50:14 +0200

    [128] Switch Mozilla suffix from "m-c" to "moz".

--- 128/128.sh
+++ 128/128.sh
@@ -4,5 +4,5 @@ MOZILLA_REV=d1f40cf63952dd0310428b72bb6771c2d6a94e7d
 COMM_REPO=https://hg.mozilla.org/comm-central/
 COMM_REV=da5e1d5dea5cc75ad506e066b5f69da0dfc6fce3
 RUST_VER=1.76.0
-MOZ="-m-c"
-MOZU="-M-C"
+MOZ="-moz"
+MOZU="-moz"
