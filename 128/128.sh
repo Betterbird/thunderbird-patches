71d5139b5cb6cba139ae02aa8c89e5b8058e45a2
Betterbird <betterbird@betterbird.eu>
2024-05-26 00:04:24 +0200

    [128] Rebase 9 patches to current M-C/C-C changesets at version 128, remove NNN14 for now.

--- 128/128.sh
+++ 128/128.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
-MOZILLA_REV=d1f40cf63952dd0310428b72bb6771c2d6a94e7d
+MOZILLA_REV=277ccd163bbc81ab9533ccaec00b91e0d49fabae
 COMM_REPO=https://hg.mozilla.org/comm-central/
-COMM_REV=da5e1d5dea5cc75ad506e066b5f69da0dfc6fce3
+COMM_REV=465c9825681252bf0c51e31893733655e335fc11
 RUST_VER=1.76.0
 MOZ="-moz"
 MOZU="-moz"
