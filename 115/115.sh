db170fd4dc1c63d0dd8c94e8295b2fcdf99956c6
Betterbird <betterbird@betterbird.eu>
2024-05-29 13:01:20 +0200

    [115] Rebase to latest comm-esr115 changeset, backout the survey (bug 1898086).

--- 115/115.sh
+++ 115/115.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
 MOZILLA_REV=36d2dde067333d0c181cb8e21b12b73c307575db
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=3eb7a47e645b97bcaa0f104f58680dd9c822c3d4
+COMM_REV=8337a1f73e49a985817eb47a1b219e823b445e85
 RUST_VER=1.70.0
 MOZ="-m-c"
 MOZU="-M-C"
