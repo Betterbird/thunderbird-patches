2e799912f60830be01776e5db7dbbaf137fcf519
Betterbird <betterbird@betterbird.eu>
2024-06-04 23:17:30 +0200

    [128] Rebase 10 patches to current M-C/C-C changesets at version 128.

--- 128/128.sh
+++ 128/128.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
-MOZILLA_REV=277ccd163bbc81ab9533ccaec00b91e0d49fabae
+MOZILLA_REV=369d5331352d27705546143e21e194b8cd88b5be
 COMM_REPO=https://hg.mozilla.org/comm-central/
-COMM_REV=465c9825681252bf0c51e31893733655e335fc11
+COMM_REV=370dfc98b74038961e914ca01257f92ec4b164e9
 RUST_VER=1.76.0
 MOZ="-moz"
 MOZU="-moz"
