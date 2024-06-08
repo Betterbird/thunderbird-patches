80a316dfbfe316d01db95f2a4633a4ca16feff68
Betterbird <betterbird@betterbird.eu>
2024-06-08 20:13:25 +0200

    [128] Rebase 6 patches to current M-C/C-C changesets at version 128. Adjust to multi-folder select.

--- 128/128.sh
+++ 128/128.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
-MOZILLA_REV=2c3e4c077e7b5a1c74947913e9c0ede4da42494b
+MOZILLA_REV=decc37f392c250b5a3aa4a7822148cdf6e990627
 COMM_REPO=https://hg.mozilla.org/comm-central/
-COMM_REV=61da4e8d6e258561aa9a31ed6f3f31187cadeb69
+COMM_REV=eec2c8c99c5fe7be6c24b28e36e1c1849478621b
 RUST_VER=1.76.0
 MOZ="-moz"
 MOZU="-moz"
