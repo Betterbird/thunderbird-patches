e2ef8158ef2302a2d071e7f80746f0921e8484a4
Betterbird <betterbird@betterbird.eu>
2024-07-01 21:29:26 +0200

    [128] Prepare for 128.0b6-bb3: Removed 1 patch upstream has now included. Rebased 1 patch.

--- 128/128.sh
+++ 128/128.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
-MOZILLA_REV=10e085c5fff8effb7bc9bc319a67c0c6f71bb601
+MOZILLA_REV=20888025784d99ba0c338253a9cb8913adb5c665
 COMM_REPO=https://hg.mozilla.org/releases/comm-beta/
-COMM_REV=9158e58d01398efcc8cabd734e9971cbabedd125
+COMM_REV=a21f28bed1fd8fc96c413200091bb8caaf150c93
 RUST_VER=1.76.0
 MOZ="-moz"
 MOZU="-moz"
