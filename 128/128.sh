bfddbab75a0a0c60298bb23789b8f8ef282d5fb0
Betterbird <betterbird@betterbird.eu>
2024-07-04 14:58:20 +0200

    [128] Swtich build to mozilla-esr128 and comm-esr128 repos.

--- 128/128.sh
+++ 128/128.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
-MOZILLA_REV=20888025784d99ba0c338253a9cb8913adb5c665
-COMM_REPO=https://hg.mozilla.org/releases/comm-beta/
-COMM_REV=a21f28bed1fd8fc96c413200091bb8caaf150c93
+MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
+MOZILLA_REV=e62c89485ef081311edf625308f0093307516d68
+COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
+COMM_REV=782271c49c206dc8a26db89bf924403ad0866cb0
 RUST_VER=1.76.0
 MOZ="-moz"
 MOZU="-moz"
