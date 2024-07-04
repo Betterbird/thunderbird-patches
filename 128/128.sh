311afa481bc1572e4585a47cb64651b657989903
Betterbird <betterbird@betterbird.eu>
2024-07-04 20:50:35 +0200

    [128] Update Rust version to 1.79.0 and MSVS version in mozconfig for Windows.

--- 128/128.sh
+++ 128/128.sh
@@ -3,6 +3,6 @@ MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
 MOZILLA_REV=e62c89485ef081311edf625308f0093307516d68
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
 COMM_REV=782271c49c206dc8a26db89bf924403ad0866cb0
-RUST_VER=1.76.0
+RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
