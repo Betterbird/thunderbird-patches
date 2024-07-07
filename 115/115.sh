ad0447f5cb966f73b7827b58d12909ca56d233b2
Betterbird <betterbird@betterbird.eu>
2024-07-07 13:30:14 +0200

    [115] Prepare for 115.13.0-bb30: Backed out beta appeal (which didn't work after all, good one!!).

--- 115/115.sh
+++ 115/115.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
-MOZILLA_REV=6b05ad1f5f2dbb0d47ac169115e250ff3776289c
+MOZILLA_REV=b7e26a68acf99da1a8a4a33f4768f18817377f9e
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=721dc287eb462999f291e62cc25552ad5848ffe7
+COMM_REV=e07fdd890dba1839d82d228fa7ceb8051d513090
 RUST_VER=1.70.0
 MOZ="-m-c"
 MOZU="-M-C"
