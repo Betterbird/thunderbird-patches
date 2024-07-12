238af7f2e054dc245dbf789c8d0a5fa40f5fb08a
Betterbird <betterbird@betterbird.eu>
2024-07-12 13:09:29 +0200

    [128] Added localisation scripts and locale de.

---
+++ 128/scripts/de.cmd
@@ -0,0 +1,184 @@
+:: "Standard" lessecho in Windows gets confused by non-ASCII characters, hence we use Git's version.
+set lecho="C:\Program Files\Git\usr\bin\lessecho.exe"
+:: Script will only work if Windows system locale is set to UTF-8 !!
+
+:: Original strings:
+:: groupButtonTooltipText=Add a new group
+%lecho% "groupButtonTooltipText=Neue Gruppe hinzufügen" >> chrome\de\locale\de\messenger\search.properties
+
+:: Original string:
+:: <!ENTITY folderContextStartupFolder.label "Startup Folder">
+%lecho% "<!ENTITY folderContextStartupFolder.label \"Startordner\">" >> chrome\de\locale\de\messenger\messenger.dtd
+
+:: Original strings:
+:: quick-filter-bar-untagged-button =
+::     .tooltiptext = Show messages with no tags
+:: quick-filter-bar-tagged-button =
+::     .tooltiptext = Show messages with one or more tags. Right-click to clear individual tag selection.
+%lecho% "quick-filter-bar-untagged-button ="                        >> localization\de\messenger\about3Pane.ftl
+%lecho% "    .tooltiptext = Nachrichten ohne Schlagwörter anzeigen" >> localization\de\messenger\about3Pane.ftl
+%lecho% "quick-filter-bar-tagged-button ="                          >> localization\de\messenger\about3Pane.ftl
+%lecho% "    .tooltiptext = Nachrichten mit einem oder mehreren Schlagwörter anzeigen. Rechts-Klick zum Löschen der Schlagwort-Auswahl." >> localization\de\messenger\about3Pane.ftl
+
+:: Original strings:
+:: enable-gloda-search-encrypted-label =
+::     .label = Enable search in encrypted messages (stores decrypted copy locally)
+%lecho% "enable-gloda-search-encrypted-label =" >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "    .label = Verschlüsselte Nachrichten durchsuchbar machen (speichert entschlüsselte Kopie lokal)" >> localization\de\messenger\preferences\preferences.ftl
+
+:: Original strings:
+:: menuitem-recent-files =
+::     .label = Recent File(s)…
+:: context-menuitem-recent-files =
+::     .label = Attach Recent File(s)…
+%lecho% "menuitem-recent-files ="                   >> localization\de\messenger\messengercompose\messengercompose.ftl
+%lecho% "    .label = Zuletzt geöffnete Datei(en)…" >> localization\de\messenger\messengercompose\messengercompose.ftl
+%lecho% "context-menuitem-recent-files ="           >> localization\de\messenger\messengercompose\messengercompose.ftl
+%lecho% "    .label = Zuletzt geöffnete Datei(en) anhängen…" >> localization\de\messenger\messengercompose\messengercompose.ftl
+
+:: Original strings:
+:: menu-multi-line-all-folders =
+::     .label = Multi-line View On All Folders
+%lecho% "menu-multi-line-all-folders ="                   >> localization\de\messenger\menubar.ftl
+%lecho% "    .label = Mehrzeilen-Ansicht für alle Ordner" >> localization\de\messenger\menubar.ftl
+
+:: Original strings:
+:: compose-send-plain-or-html-option =
+::     .label = Plain Text or HTML
+::     .accesskey = O
+:: compose-send-plain-or-html-description = If no styling is used in the message, send Plain Text. Otherwise, send HTML only.
+%lecho% "compose-send-plain-or-html-option =" >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "    .label = Reintext oder HTML"     >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "compose-send-plain-or-html-description = Falls keine Stilformatierungen verwendet werden, erfolgt die Verwendung von Reintext. Ansonsten wird nur HTML eingesetzt." >> localization\de\messenger\preferences\preferences.ftl
+
+%lecho% "compose-send-plain-or-html-menu-item =" >> localization\de\messenger\messengercompose\messengercompose.ftl
+%lecho% "    .label = Reintext oder HTML"        >> localization\de\messenger\messengercompose\messengercompose.ftl
+%lecho% "    .accesskey = O"                     >> localization\de\messenger\messengercompose\messengercompose.ftl
+
+:: Movemail
+:: Strings from https://hg.mozilla.org/l10n-central/de/file/2f346846f995f942b61562fed125b0cc98e82a13/mail/chrome/messenger/localMsgs.properties
+%lecho% "movemailCantOpenSpoolFile=Nachrichten-Warteschlangen-Datei %%S kann nicht geöffnet werden."            >> chrome\de\locale\de\messenger\localMsgs.properties
+%lecho% "movemailCantCreateLock=Die Sperrdatei %%S kann nicht erstellt werden. Für Movemail ist es notwendig, dass Sperrdateien im Nachrichten-Warteschlangen-Verzeichnis erstellt werden. Auf den meisten Systemen kann dies am besten ermöglicht werden, indem man das Nachrichten-Warteschlangen-Verzeichnis auf 01777 setzt." >> chrome\de\locale\de\messenger\localMsgs.properties
+%lecho% "movemailCantDeleteLock=Die Sperrdatei %%S kann nicht gelöscht werden."                                 >> chrome\de\locale\de\messenger\localMsgs.properties
+%lecho% "movemailCantTruncateSpoolFile=Die Warteschlangen-Datei %%S kann nicht gekürzt/zusammengefasst werden." >> chrome\de\locale\de\messenger\localMsgs.properties
+%lecho% "movemailSpoolFileNotFound=Nachrichten-Warteschlangen-Datei kann nicht gefunden werden."                >> chrome\de\locale\de\messenger\localMsgs.properties
+%lecho% "movemailCantParseSpool=Die Warteschlangen-Datei %%S konnte nicht analysiert werden. Die Datei ist möglicherweise beschädigt oder enthält unzulässige Daten." >> chrome\de\locale\de\messenger\localMsgs.properties
+%lecho% "serverType-movemail=Unix Movemail"                                                                     >> chrome\de\locale\de\messenger\messenger.properties
+
+%lecho% "account-action-add-movemail-account ="   >> localization\de\messenger\accountManager.ftl
+%lecho% "    .label = Movemail-Konto hinzufügen…" >> localization\de\messenger\accountManager.ftl
+%lecho% "    .accesskey = M"                      >> localization\de\messenger\accountManager.ftl
+%lecho% "file-new-movemail-account ="             >> localization\de\messenger\menubar.ftl
+%lecho% "    .label = Movemail-Konto…"            >> localization\de\messenger\menubar.ftl
+%lecho% "    .accesskey = M"                      >> localization\de\messenger\menubar.ftl
+
+:: Unread/seleted status
+%lecho% "unreadMsgStatus=Ungelesen: %%S"     >> chrome\de\locale\de\messenger\messenger.properties
+%lecho% "selectedMsgStatus=Ausgewählt: %%S"  >> chrome\de\locale\de\messenger\messenger.properties
+%lecho% "totalMsgStatus=Gesamt: %%S"         >> chrome\de\locale\de\messenger\messenger.properties
+
+:: Original strings:
+:: vertical-tabs-label =
+::     .label = Enable vertical tabs
+::     .accesskey = v
+:: attachments-display-top-label =
+::     .label = Display attachment list above message
+::     .accesskey = p
+%lecho% "vertical-tabs-label ="               >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "    .label = Tab-Leiste vertikal anzeigen" >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "    .accesskey = v"                  >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "attachments-display-top-label ="     >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "    .label = Anhänge oberhalb der Nachricht anzeigen" >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "    .accesskey = o"                  >> localization\de\messenger\preferences\preferences.ftl
+
+:: Original strings:
+:: restore-tabs-session-label =
+::     .value = Restore tabs on startup:
+:: restore-all-tabs =
+::     .label = All tabs
+::     .accesskey = t
+:: restore-system-tabs =
+::     .label = System tabs
+::     .accesskey = s
+:: restore-no-tabs =
+::     .label = None
+::     .accesskey = n
+%lecho% "restore-tabs-session-label ="                   >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "    .value = Tabs beim Start wiederherstellen:" >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "restore-all-tabs ="                             >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "    .label = Alle Tabs"                         >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "    .accesskey = A"                             >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "restore-system-tabs ="                          >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "    .label = System-Tabs"                       >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "    .accesskey = S"                             >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "restore-no-tabs ="                              >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "    .label = Keine"                             >> localization\de\messenger\preferences\preferences.ftl
+%lecho% "    .accesskey = K"                             >> localization\de\messenger\preferences\preferences.ftl
+
+:: Original strings:
+:: menu-view-layout-widethread =
+::     .label = Wide Thread View
+::     .accesskey = T
+:: menu-view-layout-stacked =
+::     .label = Stacked View
+::     .accesskey = S
+%lecho% "menu-view-layout-widethread ="        >> localization\de\messenger\menubar.ftl
+%lecho% "    .label = Breite Nachrichtenliste" >> localization\de\messenger\menubar.ftl
+%lecho% "    .accesskey = B"                   >> localization\de\messenger\menubar.ftl
+%lecho% "menu-view-layout-stacked ="           >> localization\de\messenger\menubar.ftl
+%lecho% "    .label = Übereinander-Ansicht"    >> localization\de\messenger\menubar.ftl
+%lecho% "    .accesskey = Ü"                   >> localization\de\messenger\menubar.ftl
+
+:: Original strings:
+:: other-action-msg-edit-headers =
+::     .label = Edit Headers…
+::     .accesskey = H
+:: msg-edit-dialog-title = Edit Headers
+:: msg-edit-cancel-button = Cancel
+:: msg-edit-restore-button = Restore
+::     .title = Show the original value
+:: msg-edit-update-button = Update
+%lecho% "other-action-msg-edit-headers ="              >> localization\de\messenger\messenger.ftl
+%lecho% "    .label = Kopfzeilen editieren…"           >> localization\de\messenger\messenger.ftl
+%lecho% "    .accesskey = K"                           >> localization\de\messenger\messenger.ftl
+%lecho% "msg-edit-dialog-title = Kopfzeilen editieren" >> localization\de\messenger\messenger.ftl
+%lecho% "msg-edit-cancel-button = Abbrechen"           >> localization\de\messenger\messenger.ftl
+%lecho% "msg-edit-restore-button = Wiederherstellen"   >> localization\de\messenger\messenger.ftl
+%lecho% "    .title = Originalwert anzeigen"           >> localization\de\messenger\messenger.ftl
+%lecho% "msg-edit-update-button = Aktualisieren"       >> localization\de\messenger\messenger.ftl
+
+:: Original strings:
+:: quick-filter-bar-negate =
+::     .title = Right-click or Ctrl+Enter on a button negates the result.
+::       For the text and tag filter, any negated button creates an AND condition.
+::       When the tag filter is negated, selecting a tag button adds respective messages to the list of untagged messages.
+::       Examples:
+::       • [Starred] and [Tags] negated: Show all messages which are Not ‘Starred’ AND Not ‘Tagged’.
+::       • [Subject] negated and [Body] selected: Show all messages where ‘Subject’ Does Not Contain “word” AND ‘Body’ Contains “word”.
+::       • [Any of] with [Important] negated and [Work] and [Personal] selected:
+::       Show all tagged messages which Do Not have the ‘Important’ tag AND which have the ‘Work’ OR the ‘Personal’ tag.
+%lecho% "quick-filter-bar-negate ="                                                                                                                >> localization\de\messenger\about3Pane.ftl
+%lecho% "    .title = Rechts-Klick oder Strg+Eingabetaste auf eine Schaltfläche negiert das Ergebnis."                                             >> localization\de\messenger\about3Pane.ftl
+%lecho% "      Für den Text- und Schlagwort-Filter erstellt jede Negierung eine UND-Bedingung."                                                    >> localization\de\messenger\about3Pane.ftl
+%lecho% "      Wenn der Schlagwort-Filter negiert ist, werden durch Auswahl eines Schlagworts entsprechende Nachrichten zur Liste hinzugefügt."    >> localization\de\messenger\about3Pane.ftl
+%lecho% "      Beispiele:"                                                                                                                         >> localization\de\messenger\about3Pane.ftl
+%lecho% "      • [Kennzeichnung] und [Schlagwort] negiert: Zeigt Nachrichten, die Nicht gekennzeichnet sind UND Keine Schlagwörter haben."         >> localization\de\messenger\about3Pane.ftl
+%lecho% "      • [Betreff] negiert und [Nachrichteninhalt] ausgewählt: Zeigt Nachrichten, bei denen ‚Betreff‘ Nicht „Wort“ enthält UND ‚Nachrichteninhalt‘ „Wort“ enthält." >> localization\de\messenger\about3Pane.ftl
+%lecho% "      • [Eins der Ausgewählten] mit [Wichtig] negiert und [Dienstlich] und [Persönlich] ausgewählt:"                                      >> localization\de\messenger\about3Pane.ftl
+%lecho% "      Zeigt Nachrichten mit Schlagwörtern, die Schlagwort ‚Wichtig‘ Nicht haben UND die Schlagwort ‚Dienstlich‘ ODER ‚Persönlich‘ haben." >> localization\de\messenger\about3Pane.ftl
+
+:: New in 128:
+
+:: Threading on menu:
+%lecho% "mail-context-unthread-message ="         >> localization\de\messenger\messenger.ftl
+%lecho% "    .label = Unthread Message"           >> localization\de\messenger\messenger.ftl
+%lecho% "    .accesskey = U"                      >> localization\de\messenger\messenger.ftl
+%lecho% "mail-context-reset-message-threading ="  >> localization\de\messenger\messenger.ftl
+%lecho% "    .label = Restore Original Threading" >> localization\de\messenger\messenger.ftl
+%lecho% "    .accesskey = R"                      >> localization\de\messenger\messenger.ftl
+
+:: Disable account:
+%lecho% "account-disabled ="          >> localization\de\messenger\messenger.ftl
+%lecho% "  .label = Account disabled" >> localization\de\messenger\messenger.ftl
+
+:: Search AB via display name and nickname (upcoming).
---
+++ 128/scripts/langpack.cmd
@@ -0,0 +1,40 @@
+mkdir langpacks
+cd langpacks
+
+:: Get language pack
+wget http://ftp.mozilla.org/pub/thunderbird/candidates/128.0esr-candidates/build3/linux-x86_64/xpi/%~1.xpi
+
+:: Remove stuff we don't want from the language pack
+7z d %~1.xpi chrome\%~1\locale\branding
+7z d %~1.xpi localization\%~1\branding
+
+:: Unpack the language pack
+7z x %~1.xpi
+rm -f %~1.xpi
+
+:: Fix the manifest.
+sed -i -e 's/langpack-%~1@thunderbird.mozilla.org/langpack-%~1@betterbird.eu/' manifest.json
+sed -i -e 's/Thunderbird Language Pack/Betterbird Language Pack/' manifest.json
+sed -i -e 's/"gecko": {/"gecko": { "update_url": "https:\/\/www.betterbird.eu\/downloads\/LanguagePacks\/updates.json",/' manifest.json
+
+:: Copy US branding
+7z x ..\betterbird-%~2.en-US.win64.installer.exe core\omni.ja
+7z x core\omni.ja chrome\en-US\locale\branding\
+7z x core\omni.ja localization\en-US\branding\
+mv chrome\en-US\locale\branding\ chrome\%~1\locale\branding\
+mv localization\en-US\branding localization\%~1\branding\
+rmdir /s /q chrome\en-US
+rmdir /s /q localization\en-US
+
+:: Call script that provides extra strings
+call ..\%~1.cmd
+
+:: Pack the whole thing again
+7z a -r -tzip %~1-%~2.xpi manifest.json chrome localization
+
+rmdir /s /q chrome
+rmdir /s /q localization
+rmdir /s /q core
+rm manifest.json
+
+cd ..
