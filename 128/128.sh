f2af44e332d89db7b1cc61d414747acfce6bfee9
Betterbird <betterbird@betterbird.eu>
2024-07-18 13:46:00 +0200

    [128] Prepare for 128.0.1esr-bb5: Removed one patch now included upstream, switched one patch to official version.

--- 128/128.sh
+++ 128/128.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
 MOZILLA_REV=4cff37e891cdf778f142fd8005c71b1dd88898b2
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=5b8f11ce4188f2d46aa8021a390e543e92a23e0d
+COMM_REV=2b846be6fb6c5aea0cc6fbcb5392b13ecdde7b08
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
