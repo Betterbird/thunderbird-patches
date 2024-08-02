548774c39cff6140fe952d7177d366b8d464a964
Betterbird <betterbird@betterbird.eu>
2024-08-02 20:25:11 +0200

    [115] Prepare for 115.14.0-bb31: Removed two patches that upstream is shipping now.

--- 115/115.sh
+++ 115/115.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
-MOZILLA_REV=b7e26a68acf99da1a8a4a33f4768f18817377f9e
+MOZILLA_REV=5983e691ecd90f7b5dbc6e7353299ebdd11a8493
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=e07fdd890dba1839d82d228fa7ceb8051d513090
+COMM_REV=99f915f469c83ab9f535e41cce590e44f6ccfa46
 RUST_VER=1.70.0
 MOZ="-m-c"
 MOZU="-M-C"
