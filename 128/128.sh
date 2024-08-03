8d681629da7288b0559ec0c5ffd2129ff64d9b94
Betterbird <betterbird@betterbird.eu>
2024-08-03 10:47:30 +0200

    [128] Prepare for 128.1.0esr-bb6: Removed six patches now included upstream, rebased two patches. Corrected rev. dad06e3 by removing folded patch from series.

--- 128/128.sh
+++ 128/128.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
-MOZILLA_REV=4cff37e891cdf778f142fd8005c71b1dd88898b2
+MOZILLA_REV=18b5deccfd5a73daddfc8a2ab5a6a41384ba8af5
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=2b846be6fb6c5aea0cc6fbcb5392b13ecdde7b08
+COMM_REV=fd08ed2b148ffec182e57d52cb15e0d424db832e
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
