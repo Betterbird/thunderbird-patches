fe80c9d176f4c75e2e10650283b5cebc60712d0f
Betterbird <betterbird@betterbird.eu>
2024-08-03 11:41:19 +0200

    [128] Switch from 128.0.1 to 128.1.0 in repack scripts.

--- 128/scripts/repack-linux.cmd
+++ 128/scripts/repack-linux.cmd
@@ -10,7 +10,7 @@ move omni.ja ..\omni.ja
 del ..\omni.ja
 
 :: Get language pack
-wget http://ftp.mozilla.org/pub/thunderbird/candidates/128.0.1esr-candidates/build2/linux-x86_64/xpi/%~1.xpi
+wget http://ftp.mozilla.org/pub/thunderbird/candidates/128.1.0esr-candidates/build1/linux-x86_64/xpi/%~1.xpi
 
 :: Remove stuff we don't want from the language pack
 7z d %~1.xpi manifest.json
