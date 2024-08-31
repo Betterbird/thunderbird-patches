9c2d3160df34633e60ccbff20c633769a4ed8394
Betterbird <betterbird@betterbird.eu>
2024-08-31 22:43:59 +0200

    [115] Prepare for 115.15.0-bb32.

--- 115/115.sh
+++ 115/115.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
-MOZILLA_REV=5983e691ecd90f7b5dbc6e7353299ebdd11a8493
+MOZILLA_REV=d4a50496fcaa7beb50ed8f63b28fa4d2be4fc779
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
-COMM_REV=99f915f469c83ab9f535e41cce590e44f6ccfa46
+COMM_REV=afc470ec4bdcfdcdd656b7779d7d90cd300e90a1
 RUST_VER=1.70.0
 MOZ="-m-c"
 MOZU="-M-C"
