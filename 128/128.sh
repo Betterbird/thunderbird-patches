dd175bd223bac477bff2063b00d240ba021af3b5
Betterbird <betterbird@betterbird.eu>
2024-09-12 20:59:27 +0200

    [128] Prepare for 128.2.1esr-bb9: Removed 12 patches now included upstream, rebased two patches.

--- 128/128.sh
+++ 128/128.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
 MOZILLA_REV=49e746f805bd2d0c6a18e9db2df6ccbac0242eae
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=4e49c98bb7a37586baa011e5a99eb5aad64ba977
+COMM_REV=1d6b847ef67fa1d35dbd4c61230545aecf2380bd
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
