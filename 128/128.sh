6a1d9cc7ee8cf3863ad05888a8a4abc16ce2c469
Betterbird <betterbird@betterbird.eu>
2024-09-17 18:37:10 +0200

    [128] Prepare for 128.2.2esr-bb10: Removed some comments from series and reshuffled some patches.

--- 128/128.sh
+++ 128/128.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
 MOZILLA_REV=49e746f805bd2d0c6a18e9db2df6ccbac0242eae
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=1d6b847ef67fa1d35dbd4c61230545aecf2380bd
+COMM_REV=7016fbe576c5b580c2b52fbd4053b3fabcde443c
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
