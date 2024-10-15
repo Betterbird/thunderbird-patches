dce1e4adedaf26fff85947e9b4ac22e26e98dcb4
Betterbird <betterbird@betterbird.eu>
2024-10-15 21:36:23 +0200

    [128] Prepare for 128.3.2esr-bb13, take 2: Removed patch which is not needed any more.

--- 128/128.sh
+++ 128/128.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
 MOZILLA_REV=e0c969a3bfc0a23219384269e5b36a589c8f6cc5
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=2b01069b799914d2289f3f4ac046d30f1e9c1859
+COMM_REV=085b0d8e7d9de5f31a94d21d0a1184694e7bc099
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
