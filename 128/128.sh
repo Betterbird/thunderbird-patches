0ab17df4f1ffa3f8a70468f223dfd8f187a70c0c
Betterbird <betterbird@betterbird.eu>
2024-10-17 12:52:22 +0200

    [128] Prepare for 128.3.2esr-bb13, take 3: Switch to comm changeset of official release.

--- 128/128.sh
+++ 128/128.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
 MOZILLA_REV=e0c969a3bfc0a23219384269e5b36a589c8f6cc5
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=085b0d8e7d9de5f31a94d21d0a1184694e7bc099
+COMM_REV=aeb2fd15a57a20f626c7de9ace7039647e8b76d7
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
