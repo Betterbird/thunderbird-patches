45537cdb436397d60a877d11e94f8d7602397d87
Betterbird <betterbird@betterbird.eu>
2024-10-27 00:17:34 +0200

    [128] Prepare for 128.4.0esr-bb15: Rebased 3 patches.

--- 128/128.sh
+++ 128/128.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
-MOZILLA_REV=e0c969a3bfc0a23219384269e5b36a589c8f6cc5
+MOZILLA_REV=c4a5d008ee64fab523384408fa2868e2d028025d
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=7791b5d54344169bb5e9c35f93868b18e40a4b5f
+COMM_REV=6c9a5fd8ba8d5956ce0cca04af0bb8f14c4c0f03
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
