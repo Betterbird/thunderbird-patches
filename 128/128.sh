0775a1b58fb95725bf795557f57d2801fa047cc4
Betterbird <betterbird@betterbird.eu>
2024-11-06 11:46:17 +0100

    [128] Prepare for 128.4.2esr-bb16: Removed 11 patches (7 bugs) that upstream is shipping now, rebased appeal patch.

--- 128/128.sh
+++ 128/128.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
 MOZILLA_REV=c4a5d008ee64fab523384408fa2868e2d028025d
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=6c9a5fd8ba8d5956ce0cca04af0bb8f14c4c0f03
+COMM_REV=ffdb226764722cd45544f945ef51e1786e1bdbbe
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
