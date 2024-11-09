9ec6f49a8c05af91ed0b30df4a460cf6c3e2a94b
Betterbird <betterbird@betterbird.eu>
2024-11-09 12:32:14 +0100

    [128] Prepare for 128.4.3esr-bb17: Removed 5 patches that upstream is shipping now, rebased 3 patches.

--- 128/128.sh
+++ 128/128.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
 MOZILLA_REV=c4a5d008ee64fab523384408fa2868e2d028025d
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=ffdb226764722cd45544f945ef51e1786e1bdbbe
+COMM_REV=145fdfb21d2ef4ad466f67bd4a410b633592ed72
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
