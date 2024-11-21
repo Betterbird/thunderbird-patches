6fca46dcc9a3247f4574f41aae8b5946427f9919
Betterbird <betterbird@betterbird.eu>
2024-11-21 10:53:54 +0100

    [115] Prepare for 115.18.0-bb36. Includes cherry-picking one bug TB included in 115.16.3.

--- 115/115.sh
+++ 115/115.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
-MOZILLA_REV=cd5c86360af44ea2bde2965209d331fedfcb639a
+MOZILLA_REV=554efae2f3d235ef2f070cb1a7df62442769f9f8
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
 COMM_REV=aa5ac5425f0381ec6c3e3d9122b5b9d14cc7e60d
 RUST_VER=1.70.0
