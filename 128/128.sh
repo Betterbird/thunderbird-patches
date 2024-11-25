d5d98b494266e9b1546447a9f885b674f63e47f4
Betterbird <betterbird@betterbird.eu>
2024-11-25 20:11:49 +0100

    [128] Prepare for 128.5.0esr-bb18: Removed 3 +3 (moz) patches that upstream is shipping now, rebased 5 patches (one including fix for bug 1926279 which upstream is shipping).

--- 128/128.sh
+++ 128/128.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
-MOZILLA_REV=c4a5d008ee64fab523384408fa2868e2d028025d
+MOZILLA_REV=68352da82b95d7114af99b268c179195fd2fead0
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=145fdfb21d2ef4ad466f67bd4a410b633592ed72
+COMM_REV=5aa787475f1ff704f48d561dd2ba209b092f8336
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
