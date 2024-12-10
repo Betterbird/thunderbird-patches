ab8186afe169ac8f6f78fdbc42f643343fc1e66a
Betterbird <betterbird@betterbird.eu>
2024-12-10 23:27:38 +0100

    [128] Prepare for 128.5.2esr-bb19, take 2: Updated version patch, rebased another patch.

--- 128/128.sh
+++ 128/128.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
 MOZILLA_REV=a6cdcd2ed9ec3e256f358010672bafd1674b0b8b
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=f6966b103721a763a6cafc2638ceecd3429f5bb2
+COMM_REV=2e403316e9cb803e5ad9bd34eab5bf812fd32186
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
