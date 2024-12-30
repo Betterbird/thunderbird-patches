64b29c8b4fd3f926d1c95f94c766567e431bfd82
Betterbird <betterbird@betterbird.eu>
2024-12-30 10:28:23 +0100

    [115] Added original strings to cs.cmd.

--- 115/scripts/cs.cmd
+++ 115/scripts/cs.cmd
@@ -2,26 +2,51 @@
 set lecho="C:\Program Files\Git\usr\bin\lessecho.exe"
 :: Script will only work if Windows system locale is set to UTF-8 !!
 
+:: Original strings:
+:: groupButtonTooltipText=Add a new group
 %lecho% "groupButtonTooltipText=Přidat novou skupinu" >> chrome\cs\locale\cs\messenger\search.properties
 
+:: Original string:
+:: <!ENTITY folderContextStartupFolder.label "Startup Folder">
 %lecho% "<!ENTITY folderContextStartupFolder.label \"Výchozí složka\">" >> chrome\cs\locale\cs\messenger\messenger.dtd
 
+:: Original strings:
+:: quick-filter-bar-untagged-button =
+::     .tooltiptext = Show messages with no tags
+:: quick-filter-bar-tagged-button =
+::     .tooltiptext = Show messages with one or more tags. Right-click to clear individual tag selection.
 %lecho% "quick-filter-bar-untagged-button ="            >> localization\cs\messenger\about3Pane.ftl
 %lecho% "    .tooltiptext = Zobrazit zprávy bez značek" >> localization\cs\messenger\about3Pane.ftl
 %lecho% "quick-filter-bar-tagged-button ="              >> localization\cs\messenger\about3Pane.ftl
 %lecho% "    .tooltiptext = Zobrazit zprávy s jednou nebo více značkami. Kliknutím pravým tlačítkem myši zrušíte výběr jednotlivých značek." >> localization\cs\messenger\about3Pane.ftl
 
+:: Original strings:
+:: enable-gloda-search-encrypted-label =
+::     .label = Enable search in encrypted messages (stores decrypted copy locally)
 %lecho% "enable-gloda-search-encrypted-label =" >> localization\cs\messenger\preferences\preferences.ftl
 %lecho% "    .label = Povolit vyhledávání v zašifrovaných zprávách (dešifrovanou kopii ukládá lokálně)" >> localization\cs\messenger\preferences\preferences.ftl
 
+:: Original strings:
+:: menuitem-recent-files =
+::     .label = Recent File(s)…
+:: context-menuitem-recent-files =
+::     .label = Attach Recent File(s)…
 %lecho% "menuitem-recent-files ="                   >> localization\cs\messenger\messengercompose\messengercompose.ftl
 %lecho% "    .label = Nedávné soubory…"             >> localization\cs\messenger\messengercompose\messengercompose.ftl
 %lecho% "context-menuitem-recent-files ="           >> localization\cs\messenger\messengercompose\messengercompose.ftl
 %lecho% "    .label = Připojte poslední soubor(y)…" >> localization\cs\messenger\messengercompose\messengercompose.ftl
 
+:: Original strings:
+:: menu-multi-line-all-folders =
+::     .label = Multi-line View On All Folders
 %lecho% "menu-multi-line-all-folders ="                        >> localization\cs\messenger\menubar.ftl
 %lecho% "    .label = Víceřádkové zobrazení ve všech složkách" >> localization\cs\messenger\menubar.ftl
 
+:: Original strings:
+:: compose-send-plain-or-html-option =
+::     .label = Plain Text or HTML
+::     .accesskey = O
+:: compose-send-plain-or-html-description = If no styling is used in the message, send Plain Text. Otherwise, send HTML only.
 %lecho% "compose-send-plain-or-html-option =" >> localization\cs\messenger\preferences\preferences.ftl
 %lecho% "    .label = Prostý text nebo HTML"  >> localization\cs\messenger\preferences\preferences.ftl
 %lecho% "compose-send-plain-or-html-description = Pokud není ve zprávě použit žádný styl, odešlete prostý text. V opačném případě odešlete pouze HTML." >> localization\cs\messenger\preferences\preferences.ftl
@@ -47,10 +72,18 @@ set lecho="C:\Program Files\Git\usr\bin\lessecho.exe"
 %lecho% "    .label = Účet Movemail…"           >> localization\cs\messenger\menubar.ftl
 %lecho% "    .accesskey = M"                    >> localization\cs\messenger\menubar.ftl
 
+:: Unread/seleted status
 %lecho% "unreadMsgStatus=Nepřečtené: %%S" >> chrome\cs\locale\cs\messenger\messenger.properties
 %lecho% "selectedMsgStatus=Vybrané: %%S"  >> chrome\cs\locale\cs\messenger\messenger.properties
 %lecho% "totalMsgStatus=Celkem: %%S"      >> chrome\cs\locale\cs\messenger\messenger.properties
 
+:: Original strings:
+:: vertical-tabs-label =
+::     .label = Enable vertical tabs
+::     .accesskey = v
+:: attachments-display-top-label =
+::     .label = Display attachment list above message
+::     .accesskey = p
 %lecho% "vertical-tabs-label ="                             >> localization\cs\messenger\preferences\preferences.ftl
 %lecho% "    .label = Zobrazení pásu karet na výšku"        >> localization\cs\messenger\preferences\preferences.ftl
 %lecho% "    .accesskey = v"                                >> localization\cs\messenger\preferences\preferences.ftl
