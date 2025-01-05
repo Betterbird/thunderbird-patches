128f45c017df58aad7a61f5b47aa1b4e8d7b76f9
Betterbird <betterbird@betterbird.eu>
2025-01-05 19:09:08 +0100

    [115] Prepare for 115.19.0-bb37.

--- 115/115.sh
+++ 115/115.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr115/
-MOZILLA_REV=554efae2f3d235ef2f070cb1a7df62442769f9f8
+MOZILLA_REV=36fd7b6b3ddbffe940e402af3c6d1b44ce285543
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr115/
 COMM_REV=aa5ac5425f0381ec6c3e3d9122b5b9d14cc7e60d
 RUST_VER=1.70.0
