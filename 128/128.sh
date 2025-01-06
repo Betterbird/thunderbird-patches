eae0b059515d1c17c93f5ba18b1e1403f9b73bea
Betterbird <betterbird@betterbird.eu>
2025-01-06 12:17:13 +0100

    [128] Prepare for 128.6.0esr-bb20: Removed 4 patches that upstream is shipping now, rebased 6 patches.

--- 128/128.sh
+++ 128/128.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
-MOZILLA_REV=a6cdcd2ed9ec3e256f358010672bafd1674b0b8b
+MOZILLA_REV=4f008c71b12e001ae54b7fcd4787b266764c28bf
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=2e403316e9cb803e5ad9bd34eab5bf812fd32186
+COMM_REV=24eeb777219982df59786e6423a4a8f1f55ca146
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
