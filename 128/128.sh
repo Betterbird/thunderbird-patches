2d4d36eb4ebc57bfce786d57ab3d21189b3613f7
Betterbird <betterbird@betterbird.eu>
2025-01-22 17:40:48 +0100

    [128] Prepare for 128.6.1esr-bb21, take 2: Switch to build2 changeset and cherry-pick two more bugs.

--- 128/128.sh
+++ 128/128.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
 MOZILLA_REV=4f008c71b12e001ae54b7fcd4787b266764c28bf
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=22de9e80f2cfb506358010c57a76e30096ea07aa
+COMM_REV=e978da331e230b2ccbc223e68ec44ed7a07bfaf9
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
