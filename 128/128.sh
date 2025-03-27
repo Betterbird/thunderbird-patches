54806659c4d329448019f3fec002b17a2b292b94
Betterbird <betterbird@betterbird.eu>
2025-03-27 22:44:32 +0100

    [128] Prepare for 128.9.0esr-bb24.

--- 128/128.sh
+++ 128/128.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
-MOZILLA_REV=c685d5844a0e4f99ac535b6ffc641fbd07696c68
+MOZILLA_REV=3aaf665616b3c0f61839d40a02a9f7ae8778b04c
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=f8183c5232ec457f3c8b5be36c7891d933bbb457
+COMM_REV=fd8e6824bea90e5407097ae3f27277f6f456d79b
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
