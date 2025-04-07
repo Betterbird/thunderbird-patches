852d9956f00cf16faa19b82dcead154dcf40c57b
Betterbird <betterbird@betterbird.eu>
2025-04-07 22:56:23 +0200

    [release] Add 19 MOZ patches (one missing).

---
+++ release/release.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
+MOZILLA_REV=56754e1716a679d7a19b4c7a48b3ee2fa86d847b
+COMM_REPO=https://hg.mozilla.org/comm-central/
+COMM_REV=5766fb44cd37d54ebd76ddd41b4a1623caf1d26d
+RUST_VER=1.86.0
+MOZ="-moz"
+MOZU="-moz"
