de2fd80e5f98d5e6b82134c3e448896b906b4106
Betterbird <betterbird@betterbird.eu>
2025-04-16 00:00:55 +0200

    [128] Prepare for 128.9.2esr-bb25: Shuffle 4 branding patches up, remove 2 patches upstream is shipping now, rebase EOY appeal patch.

--- 128/128.sh
+++ 128/128.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
 MOZILLA_REV=3aaf665616b3c0f61839d40a02a9f7ae8778b04c
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=ac4a25a5a9f043aeece186ccb593f54800b562cc
+COMM_REV=3dc116e792891bdcfaaa36232ed71fb2a79ed570
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
