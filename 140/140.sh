79b193de66b1d7a24f9c732f441b14102e3c2c61
Betterbird <betterbird@betterbird.eu>
2025-04-29 20:45:42 +0200

    [140] Switch to current M-C/C-C changesets at version 140.

--- 140/140.sh
+++ 140/140.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
-MOZILLA_REV=84855b5131dbbb012e969a160d2f2c691b4de0db
+MOZILLA_REV=c606c420560736c2182e2664024067058dd497ae
 COMM_REPO=https://hg.mozilla.org/comm-central/
-COMM_REV=3d9b0557e74a2fdfefaf16e008279e1a497ff49f
+COMM_REV=cb80efd55b6887bc8e918d7fc939b34f811997f0
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
