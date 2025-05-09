b981148e152c157656dcfaee97fe3bcfeedc7012
Betterbird <betterbird@betterbird.eu>
2025-05-09 21:43:07 +0200

    [140] Switch to current M-C/C-C changesets at version 140, rebase 1 patch, drop 1 patch now included upstream.

--- 140/140.sh
+++ 140/140.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
-MOZILLA_REV=abe9dfdecc4f5ef56ff58814a337fae1d57bdf52
+MOZILLA_REV=bc20f347807af041519d634ec48576d92066e6f6
 COMM_REPO=https://hg.mozilla.org/comm-central/
-COMM_REV=09afffe3238b75cc457d89df7cbbaf9e170dc842
+COMM_REV=566b88392bb6e952e68758e163c79a257f1a2d06
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
