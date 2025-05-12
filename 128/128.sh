4b37a77725114da2e6657a0c943b81b548507764
Betterbird <betterbird@betterbird.eu>
2025-05-13 01:29:33 +0200

    [128] Prepare for 128.10.1esr-bb27, take 1: Remove 4 patches that upstream is shipping now, rebase 1 patch.

--- 128/128.sh
+++ 128/128.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
 MOZILLA_REV=8e875e7de75c05112284e9f6e91927b7bbe36e79
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=fb57b123f3f34cc4fdca17761f3743351afee769
+COMM_REV=1e7297c0123c0a419fc59d4c8b8a4c4a4c4f6af2
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
