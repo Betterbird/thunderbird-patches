a4e0c5c09115be5e0988d87b0cd7aa659b952f17
Betterbird <betterbird@betterbird.eu>
2025-05-13 10:59:15 +0200

    [128] Prepare for 128.10.1esr-bb27, take 2: Switch to final changeset.

--- 128/128.sh
+++ 128/128.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr128/
 MOZILLA_REV=8e875e7de75c05112284e9f6e91927b7bbe36e79
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr128/
-COMM_REV=1e7297c0123c0a419fc59d4c8b8a4c4a4c4f6af2
+COMM_REV=88d37ec175b0fd667783201820dc364088216fcc
 RUST_VER=1.79.0
 MOZ="-moz"
 MOZU="-moz"
