25ffe3837d1f8652b83c8d4c4a982cf6214aa352
Betterbird <betterbird@betterbird.eu>
2025-05-13 21:07:25 +0200

    [128] Switch from 128.10.0 to 128.10.1 in repack scripts and metadata.

--- 128/scripts/langpack.cmd
+++ 128/scripts/langpack.cmd
@@ -2,7 +2,7 @@ mkdir langpacks
 cd langpacks
 
 :: Get language pack
-wget http://ftp.mozilla.org/pub/thunderbird/candidates/128.10.0esr-candidates/build1/linux-x86_64/xpi/%~1.xpi
+wget http://ftp.mozilla.org/pub/thunderbird/candidates/128.10.1esr-candidates/build1/linux-x86_64/xpi/%~1.xpi
 
 :: Remove stuff we don't want from the language pack
 7z d %~1.xpi chrome\%~1\locale\branding
