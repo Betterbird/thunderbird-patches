de7245a028f488573e097a28d00f4ced555bbdd4
Betterbird <betterbird@betterbird.eu>
2025-05-27 22:54:08 +0200

    [140] Move 140 build from comm-central to comm-beta, take 1 (won't build like this).

--- 140/140.sh
+++ 140/140.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
-MOZILLA_REV=44e4e213379b4264a44d0103ef2c3d07db295a04
-COMM_REPO=https://hg.mozilla.org/comm-central/
-COMM_REV=2cfb837458b4b96cb3ba08495532cc1cefb00a1e
+MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
+MOZILLA_REV=c7ce48a6d63d032a766b8e0fd4018f9ea2e59868
+COMM_REPO=https://hg.mozilla.org/releases/comm-beta/
+COMM_REV=4ccf36c2997f5e7da5f622682c1048fb8d70e302
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
