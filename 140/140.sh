1d10e5188968ec78d4cc2ccd10619deca8f7aecd
Betterbird <betterbird@betterbird.eu>
2025-06-06 21:19:50 +0200

    [140] Switch to current M-B/C-B changesets at version 140, rebase 2 patches.

--- 140/140.sh
+++ 140/140.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
-MOZILLA_REV=c42f704bd6c40f1f596ffbbb248aa816380e0697
+MOZILLA_REV=35051cdeb8bf16c0019f4ad3a9f7b2437def670c
 COMM_REPO=https://hg.mozilla.org/releases/comm-beta/
-COMM_REV=b5ec7080e84550a866fdec9bdad9af361df50264
+COMM_REV=868b130f3c52918c815eafb21fbedd86b67a71d7
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
