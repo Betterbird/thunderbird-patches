152aaba6818a3766afeccdf4f732b7b7eaa093ee
Betterbird <betterbird@betterbird.eu>
2025-06-24 13:21:47 +0200

    [140] Switch to current official changeset and drop patch which is now included upstream.

--- 140/140.sh
+++ 140/140.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr140/
 MOZILLA_REV=c2cc55e4c6ab5f0b8d99ec3e60fdf742c4d8ff65
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr140/
-COMM_REV=6f72eb77d1858f6deb2fc82f0f194914a57fdf1e
+COMM_REV=cdbbf82be00b28b2637d65978d590723edddd8b6
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
