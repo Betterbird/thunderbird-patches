533fc861eef4e8b08fb002728b9248d7f5747b2f
Betterbird <betterbird@betterbird.eu>
2025-07-19 00:22:26 +0200

    [140] Prepare for 140.1.0esr-bb7: Removed 1 patch that upstream is shipping now.

--- 140/140.sh
+++ 140/140.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr140/
-MOZILLA_REV=c2cc55e4c6ab5f0b8d99ec3e60fdf742c4d8ff65
+MOZILLA_REV=0c53463d0e61c036c08be46403e896e174f2182e
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr140/
-COMM_REV=9c4fe7df934f3a2df9713f83df9c4e39e27d4535
+COMM_REV=8117083abebdd537d63169fa478a305c08231d30
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
