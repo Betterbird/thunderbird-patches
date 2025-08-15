8e7b08f3c9f763829af2b6ab8459692cdfa157e0
Betterbird <betterbird@betterbird.eu>
2025-08-15 11:11:31 +0200

    [140] Prepare for 140.2.0esr-bb9: Removed 2 patches that upstream is shipping now. switched one moz patch to official version.

--- 140/140.sh
+++ 140/140.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr140/
-MOZILLA_REV=0c53463d0e61c036c08be46403e896e174f2182e
+MOZILLA_REV=a511f36cca85d35bd9989b4a3902556b664d9e13
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr140/
-COMM_REV=75f165a072f3c1f6ff56dda47ec78127d06bad43
+COMM_REV=b5f9e01e59acaefc030248bc08ea2a31d536d541
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
