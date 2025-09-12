aed6e8cd3b5651979bb356135042db98dcc23102
Betterbird <betterbird@betterbird.eu>
2025-09-12 22:09:52 +0200

    [140] Prepare for 140.3.0esr-bb11: Removed 3 patches that upstream is shipping now.

--- 140/140.sh
+++ 140/140.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr140/
-MOZILLA_REV=a511f36cca85d35bd9989b4a3902556b664d9e13
+MOZILLA_REV=21285e5fdf030ee5df4e371b921ba4d985477aff
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr140/
-COMM_REV=5df036ddfe367ff4452a4aa6235bb652dca89fa4
+COMM_REV=34b243658c31506d293b13d67238ccca56c290e0
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
