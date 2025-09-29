dbe651dce21296812fb93d938fd0e2d1853f04a1
Betterbird <betterbird@betterbird.eu>
2025-09-29 15:12:51 +0200

    [140] Prepare for 140.3.1esr-bb12: Removed 6 patches that upstream is shipping now, shuffle cherry-picks up, rebase 4 patches.

--- 140/140.sh
+++ 140/140.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr140/
-MOZILLA_REV=21285e5fdf030ee5df4e371b921ba4d985477aff
+MOZILLA_REV=0b8c16d258d0c9d9d0379b9f317c77245ec010ff
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr140/
-COMM_REV=34b243658c31506d293b13d67238ccca56c290e0
+COMM_REV=d9e2f176f4b31eba803b25cd3d3280beb81b2b3a
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
