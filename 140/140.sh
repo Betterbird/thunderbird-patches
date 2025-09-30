b2af9e1ca4b7d65e90d6420a421f9bbc711a600b
Betterbird <betterbird@betterbird.eu>
2025-09-30 10:53:52 +0200

    [140] Prepare for 140.3.1esr-bb12, take 2: Switch to official release changeset.

--- 140/140.sh
+++ 140/140.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr140/
 MOZILLA_REV=0b8c16d258d0c9d9d0379b9f317c77245ec010ff
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr140/
-COMM_REV=d9e2f176f4b31eba803b25cd3d3280beb81b2b3a
+COMM_REV=92eb5225318ba6425cd177e8ffda37917fbb9444
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
