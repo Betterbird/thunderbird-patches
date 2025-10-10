c156a57c6f821a14a70a27de65ff9e9513fe17ba
Betterbird <betterbird@betterbird.eu>
2025-10-10 13:27:00 +0200

    [140] Prepare for 140.4.0esr-bb13: Removed 1 patch that upstream is shipping now, corrected 1 wrong backport, rebased 2 patches.

--- 140/140.sh
+++ 140/140.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr140/
-MOZILLA_REV=0b8c16d258d0c9d9d0379b9f317c77245ec010ff
+MOZILLA_REV=c4525dfe35a20458c7a6966c0c6d27f92f2deca7
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr140/
-COMM_REV=92eb5225318ba6425cd177e8ffda37917fbb9444
+COMM_REV=efb07defaa2d56105675dc1d936af581ebfd8ffa
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
