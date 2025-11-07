112c641a7e9a27cf1984285874841b699ca9667e
Betterbird <betterbird@betterbird.eu>
2025-11-07 19:43:44 +0100

    Prepare for 140.5.0esr-bb14: Removed 4 patches that upstream is shipping now, switched 1 patch to official Mozilla version.

--- 140/140.sh
+++ 140/140.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr140/
-MOZILLA_REV=c4525dfe35a20458c7a6966c0c6d27f92f2deca7
+MOZILLA_REV=558705980ca9db16de0564b5a6031b5d6e0a7efe
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr140/
-COMM_REV=efb07defaa2d56105675dc1d936af581ebfd8ffa
+COMM_REV=de7203e78b3a2a2182be3eea765b7082e4eac66f
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
