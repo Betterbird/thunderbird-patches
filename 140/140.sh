454743e56a64376c9f85d929fa2821c4e6fe40bc
Betterbird <betterbird@betterbird.eu>
2025-11-08 10:24:08 +0100

    Prepare for 140.5.0esr-bb14, take 2: Switch to final comm-esr140 changeset.

--- 140/140.sh
+++ 140/140.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr140/
 MOZILLA_REV=558705980ca9db16de0564b5a6031b5d6e0a7efe
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr140/
-COMM_REV=de7203e78b3a2a2182be3eea765b7082e4eac66f
+COMM_REV=6a3011b7161c6f3a36d5116f2608d51b19fb4d58
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
