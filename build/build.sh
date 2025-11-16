710332c6b2548c18bf433966f9e774455602a002
Betterbird <betterbird@betterbird.eu>
2025-11-16 15:34:22 +0100

    [build] Replace `uname -i` with `uname -m` and simplify the logic.

--- build/build.sh
+++ build/build.sh
@@ -52,20 +52,19 @@ if [ "$VERSION" -ge 140 ]; then
 else
   ARCHIVE_SUFFIX="tar.bz2"
 fi
+
 UNAME=$(uname)
-# uname -i doesn't work on Mac.
-UNAME_ARCH="unknown"
-if [ "$UNAME" = "Linux" ]; then
+# Note: uname -i is non-portable, even across GNU/Linux distributions, and it doesn't work on Mac.
+UNAME_ARCH=$(uname -m)
+
+# Hack for Debian 11 Bullseye on Amazon EC2 image.
+if [ "$UNAME" = "Linux" ] && [ "$UNAME_ARCH" = "unknown" ]; then
   UNAME_FULL=$(uname -a)
-  UNAME_ARCH=$(uname -i)
-  # Hack for Debian 11 Bullseye on Amazon EC2 image.
-  if [[ "$UNAME_FULL" == *"aarch64"* ]] && [ "$UNAME_ARCH" = "unknown" ]; then
+  if [[ "$UNAME_FULL" == *"aarch64"* ]]; then
     UNAME_ARCH="aarch64"
   fi
 fi
-if [ "$UNAME" = "Darwin" ]; then
-  UNAME_ARCH=$(uname -m)
-fi
+
 # Windows: Cater for newer and older MozillaBuild: MINGW32_NT-6.2 or MSYS_NT-10
 if [[ "$UNAME" == *"_NT-"* ]]; then
   UNAME="Windows"
