6ac9b284750e768075db28359baf41b0a9645389
Betterbird <betterbird@betterbird.eu>
2025-12-05 22:23:00 +0100

    Prepare for 140.6.0esr-bb15 and reshuffle series-moz a bit.

--- 140/140.sh
+++ 140/140.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr140/
-MOZILLA_REV=558705980ca9db16de0564b5a6031b5d6e0a7efe
+MOZILLA_REV=18556c0b079c839f4d15597a57b0f048fdadcedd
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr140/
-COMM_REV=6a3011b7161c6f3a36d5116f2608d51b19fb4d58
+COMM_REV=bdcc941c35b0b6de74ee4b44c694aebf5507c5cd
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
