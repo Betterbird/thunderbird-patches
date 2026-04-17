20c48c4d0e6a12e65be06a305e453922973b6254
Betterbird <betterbird@betterbird.eu>
2026-04-17 16:54:48 +0200

    Prepare for 140.10.0esr-bb21.

--- 140/140.sh
+++ 140/140.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr140/
-MOZILLA_REV=ad4a5ceb5516151b4014e2af6c1565c68ab330cc
+MOZILLA_REV=a1c8c4081ea0e227dbde64c95ec6942fa71edb28
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr140/
-COMM_REV=8be9f2d3072c225a1e5f153745a18984c2babf8f
+COMM_REV=b23998b7b404ff8144a931ada5d315103d0cd932
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
