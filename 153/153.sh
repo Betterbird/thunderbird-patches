26ee3dda0f559c7eefa219f72d0e24fdcabb8c29
Betterbird <betterbird@betterbird.eu>
2026-05-19 00:50:00 +0200

    [153] Switch to current M-C/C-C changesets at version 153, rebase 1 patch.

--- 153/153.sh
+++ 153/153.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
-MOZILLA_REV=4201ad8bbb183a6ae00df4cfd5d775a976564828
+MOZILLA_REV=f6f30a6f9b1eaaa8b8dd660933060beda7a12d1b
 COMM_REPO=https://hg.mozilla.org/comm-central/
-COMM_REV=cadc38913d17d7b46fa8cb715bc5aa1f01751791
+COMM_REV=c4b498db0747ad64a3aee54d3bb36f1632761b48
 RUST_VER=1.95.0
 MOZ="-moz"
 MOZU="-moz"
