eafdb10b7dbe1560d826747f3a1e83a8ed02eb1f
Betterbird <betterbird@betterbird.eu>
2026-06-11 21:01:14 +0200

    [153] Switch to C-C changeset b7634da2016f (Bug 1882368 : account colours for cards view) and rebase two account colours patches.

--- 153/153.sh
+++ 153/153.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
 MOZILLA_REV=410349357653aa70df8fc9d1e8ff0d8b4c50a78f
 COMM_REPO=https://hg.mozilla.org/comm-central/
-COMM_REV=7a2157b4203fec018e61379f4bc34aa3e350f47d
+COMM_REV=b7634da2016fe5b239473b2ac766bbaade07c752
 RUST_VER=1.95.0
 MOZ="-moz"
 MOZU="-moz"
