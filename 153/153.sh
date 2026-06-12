d2fcfd7ed253fa990dfbb2871dfe1136e2671aa7
Betterbird <betterbird@betterbird.eu>
2026-06-12 13:34:31 +0200

    [153] Switch to current M-C/C-C changesets at version 153. Rebase 4 patches: Eclipse->Meadow, hostName->hostname.

--- 153/153.sh
+++ 153/153.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
-MOZILLA_REV=410349357653aa70df8fc9d1e8ff0d8b4c50a78f
+MOZILLA_REV=b2c97442595f3b7da4fcdc90036c01851c9485e6
 COMM_REPO=https://hg.mozilla.org/comm-central/
-COMM_REV=b7634da2016fe5b239473b2ac766bbaade07c752
+COMM_REV=9400ab220c46b05ed3f51d9665c48561bc81f4b4
 RUST_VER=1.95.0
 MOZ="-moz"
 MOZU="-moz"
