8e8cf397bc46921d5b579a07af0770fefd4a61c2
Betterbird <betterbird@betterbird.eu>
2026-06-15 23:45:56 +0200

    [153] Move 153 build from comm-central to comm-beta: Fix repos, branding, disable debug.

--- 153/153.sh
+++ 153/153.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-MOZILLA_REPO=https://hg.mozilla.org/mozilla-central/
-MOZILLA_REV=dc23a6a0dbc999a0ec38e7ce87f654945568b28b
-COMM_REPO=https://hg.mozilla.org/comm-central/
-COMM_REV=e71837830e0948fddc0102c73eba963aa89380a8
+MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
+MOZILLA_REV=6f15dfa10e9d90d36271dfe040f005c190c5a27b
+COMM_REPO=https://hg.mozilla.org/releases/comm-beta/
+COMM_REV=87c1c5d49e6b722e39ead1426c35776e60da74c3
 RUST_VER=1.95.0
 MOZ="-moz"
 MOZU="-moz"
