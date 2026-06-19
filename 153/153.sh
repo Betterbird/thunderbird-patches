4d510e03e27b2e4033695791b810077b8852c5d9
Betterbird <betterbird@betterbird.eu>
2026-06-19 10:55:41 +0200

    [150] 153.0b1-bb1: Switch to official release changeset.

--- 153/153.sh
+++ 153/153.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
 MOZILLA_REV=6f15dfa10e9d90d36271dfe040f005c190c5a27b
 COMM_REPO=https://hg.mozilla.org/releases/comm-beta/
-COMM_REV=7b1fb5b63947630423e833d7811c4b5b50f40daf
+COMM_REV=e2c878ff166324a9b3b85f95bb8a56cac439c598
 RUST_VER=1.95.0
 MOZ="-moz"
 MOZU="-moz"
