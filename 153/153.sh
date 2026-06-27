6f191681c8e7a83020e0232ad3aeae3fb69cd01c
Betterbird <betterbird@betterbird.eu>
2026-06-27 09:51:23 +0200

    [153] Prepare for 153.0b4-bb3.

--- 153/153.sh
+++ 153/153.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
-MOZILLA_REV=225028a07cb5a0d3bca550766ef948832f05af24
+MOZILLA_REV=39a965fa858802e4517f52817d8a161a8ed795a5
 COMM_REPO=https://hg.mozilla.org/releases/comm-beta/
-COMM_REV=e95776e4bae0376652836599dfe3d4cab10838bc
+COMM_REV=c171ce338cf8c11cbab414997209c6622e0ede3e
 RUST_VER=1.95.0
 MOZ="-moz"
 MOZU="-moz"
