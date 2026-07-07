63a07027a5c7a927eda619224a2a9053f8cd5ca9
Betterbird <betterbird@betterbird.eu>
2026-07-07 21:14:01 +0200

    [153] Prepare for 153.0b6-bb4: Drop one patch TB is shipping now.

--- 153/153.sh
+++ 153/153.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
-MOZILLA_REV=39a965fa858802e4517f52817d8a161a8ed795a5
+MOZILLA_REV=7fb41d24be799d3ce8fa8a17564ebc335c0b54a0
 COMM_REPO=https://hg.mozilla.org/releases/comm-beta/
-COMM_REV=c171ce338cf8c11cbab414997209c6622e0ede3e
+COMM_REV=e2f0ad743d9c53839dea09447043ead8a0af56ae
 RUST_VER=1.95.0
 MOZ="-moz"
 MOZU="-moz"
