bbc30a77f54bd364d9f4081c00a02ee3c8f51239
Betterbird <betterbird@betterbird.eu>
2026-07-15 11:13:11 +0200

    [153] Prepare for 153.0b9-bb5, take 2: Switch to official changeset.

--- 153/153.sh
+++ 153/153.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-beta/
 MOZILLA_REV=e838658c28b358b4a4fd9beda025f3ec62129987
 COMM_REPO=https://hg.mozilla.org/releases/comm-beta/
-COMM_REV=a3ee8de8c31f8ef8d02ebb86616515a8b94faa78
+COMM_REV=35291b8f66bca1e61877ef7e20c535318f31d8c9
 RUST_VER=1.95.0
 MOZ="-moz"
 MOZU="-moz"
