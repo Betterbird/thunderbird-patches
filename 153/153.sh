14becbe146a86a690d24ce0a7782bdcc15a13bb6
Betterbird <betterbird@betterbird.eu>
2026-07-18 14:54:21 +0200

    [153] Move 153 build from comm-beta to comm-esr153, take 3.

--- 153/153.sh
+++ 153/153.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr153/
-MOZILLA_REV=b2b2b86e162751d4d5592a57d5df387f4be69f10
+MOZILLA_REV=abd1fe67f4c3b677caecb582d55601a7b1191dc4
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr153/
 COMM_REV=0b6dbdd8c79347fd1b87787dcebb1a12b5f6a7a4
 RUST_VER=1.95.0
