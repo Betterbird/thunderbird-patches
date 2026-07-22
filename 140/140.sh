a72e7ce5658f7c8880b9076f0fa0080ec666cfd4
Betterbird <betterbird@betterbird.eu>
2026-07-22 10:29:09 +0200

    [140] Switch 140.13.0 to really be built from the official 140.13.0 tarball.

--- 140/140.sh
+++ 140/140.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr140/
 MOZILLA_REV=611eeda342a90baf6918fe754fcf168087b3767e
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr140/
-COMM_REV=7b16fa0b13811dc1bd97613971a6ac37057c9d87
+COMM_REV=faf78db7dfb184002adc6b859c8ad395ff216239
 RUST_VER=1.86.0
 MOZ="-moz"
 MOZU="-moz"
