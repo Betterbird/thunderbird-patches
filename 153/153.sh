7fcf70fdb5f21de08d41d500a96738fb5be974bd
Betterbird <betterbird@betterbird.eu>
2026-08-18 09:22:08 +0200

    [153] Prepare for 153.1.0esr-bb7, take 2: Switch to official release changeset.

--- 153/153.sh
+++ 153/153.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr153/
 MOZILLA_REV=bdb74c45c2e1e3fe593fbf3c5ca6d2ab2046ef08
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr153/
-COMM_REV=be5d2684121838e5f66e43df95dc255b4f1c3e87
+COMM_REV=05d0149d7b2592412ce7c50954a4131844f6489a
 RUST_VER=1.95.0
 MOZ="-moz"
 MOZU="-moz"
