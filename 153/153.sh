f2b0b937097eb6068bdc16f81e338ccd02a0da01
Betterbird <betterbird@betterbird.eu>
2026-09-15 00:06:52 +0200

    Prepare for 153.3.0esr-bb9: Remove 8 patches that upstream is shipping now (further 4 moz patches to be removed later). Rebase 2 patches.

--- 153/153.sh
+++ 153/153.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr153/
-MOZILLA_REV=92c5bf513a3e4e39fa70df2df6a565a1049a9920
+MOZILLA_REV=3b49a44994fb7ff6902f7df5a3bd5f689a4fb4e9
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr153/
-COMM_REV=094cc105ce05f1828283b9961d3310f4aef3a1d4
+COMM_REV=03b6951f02c919226dac90f574a53cbbbb365720
 RUST_VER=1.95.0
 MOZ="-moz"
 MOZU="-moz"
