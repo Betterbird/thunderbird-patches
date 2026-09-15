bb5d5785dd92ab26390b2f0dd687e0d0610280a2
Betterbird <betterbird@betterbird.eu>
2026-09-15 21:47:51 +0200

    [153] Prepare for 153.3.0esr-bb9, take 2: Switch to official release changeset (build 2).

--- 153/153.sh
+++ 153/153.sh
@@ -2,7 +2,7 @@
 MOZILLA_REPO=https://hg.mozilla.org/releases/mozilla-esr153/
 MOZILLA_REV=3b49a44994fb7ff6902f7df5a3bd5f689a4fb4e9
 COMM_REPO=https://hg.mozilla.org/releases/comm-esr153/
-COMM_REV=03b6951f02c919226dac90f574a53cbbbb365720
+COMM_REV=bf28209ccd36622a71e5e6d79c18fcf3c0fdab80
 RUST_VER=1.95.0
 MOZ="-moz"
 MOZU="-moz"
